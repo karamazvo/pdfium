5d323f827958f8fa398195221e0daea685932ca7
Lei Zhang <thestig@chromium.org>
2024-06-17 16:56:42 -0700

    Make CPDF_PageLabel::GetLabel() faster
    Implement CPDF_NumberTree::GetLowerBound(), so that
    CPDF_PageLabel::GetLabel() can find the value it needs in 1 search of
    the number tree, instead of N. With this speed up, enable
    PageLabelTest.GetLabelPerf, which now runs in ~1 second instead of ~200.
    
    Since CPDF_PageLabel is the implementation for FPDF_GetPageLabel(), the
    public API is faster too.
    
    Bug: 347268873
    Change-Id: Ie64d94be78227b975db897411c386de1babf7065

--- core/fpdfdoc/cpdf_numbertree.h
+++ core/fpdfdoc/cpdf_numbertree.h
@@ -7,6 +7,8 @@
 #ifndef CORE_FPDFDOC_CPDF_NUMBERTREE_H_
 #define CORE_FPDFDOC_CPDF_NUMBERTREE_H_
 
+#include <optional>
+
 #include "core/fxcrt/retain_ptr.h"
 
 class CPDF_Dictionary;
@@ -14,10 +16,30 @@ class CPDF_Object;
 
 class CPDF_NumberTree {
  public:
+  struct KeyValue {
+    KeyValue(int key, RetainPtr<const CPDF_Object> value);
+    KeyValue(KeyValue&) = delete;
+    KeyValue& operator=(KeyValue&) = delete;
+    KeyValue(KeyValue&&) noexcept;
+    KeyValue& operator=(KeyValue&&) noexcept;
+    ~KeyValue();
+
+    int key;
+    RetainPtr<const CPDF_Object> value;
+  };
+
   explicit CPDF_NumberTree(RetainPtr<const CPDF_Dictionary> root);
   ~CPDF_NumberTree();
 
+  // Finds the object in the number tree whose key is `num`. Returns nullptr in
+  // there is no `num` key in the tree.
   RetainPtr<const CPDF_Object> LookupValue(int num) const;
+
+  // Finds the object in the number tree with the largest key, such that
+  // `num` >= key. Returns the key/value pair if such a key exists, or
+  // std::nullopt otherwise.
+  // Note that this is not the same as std::lower_bound().
+  std::optional<KeyValue> GetLowerBound(int num) const;
 
  protected:
   RetainPtr<const CPDF_Dictionary> const root_;
