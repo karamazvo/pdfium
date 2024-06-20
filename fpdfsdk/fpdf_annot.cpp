e3e2e41e29cace6cdfb3b1bfb079b98efc0f7828
Miklos Vajna <miklos.vajna@collabora.com>
2024-06-20 14:29:28 +0200

    Add FPDFAnnot_GetFontColor API
    This is similar to FPDFAnnot_GetFontSize() and allows getting the
    font/text color of an annotation widget.
    
    Change-Id: I7ec83a5eef8277073154b14288e271b0fed0ba59

--- fpdfsdk/fpdf_annot.cpp
+++ fpdfsdk/fpdf_annot.cpp
@@ -1366,6 +1366,48 @@ FPDFAnnot_GetFontSize(FPDF_FORMHANDLE hHandle,
   return true;
 }
 
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFAnnot_GetFontColor(FPDF_FORMHANDLE hHandle,
+                       FPDF_ANNOTATION annot,
+                       unsigned int* R,
+                       unsigned int* G,
+                       unsigned int* B) {
+  if (!R || !G || !B) {
+    return false;
+  }
+
+  CPDFSDK_InteractiveForm* pForm = FormHandleToInteractiveForm(hHandle);
+  if (!pForm) {
+    return false;
+  }
+
+  const CPDF_Dictionary* pAnnotDict = GetAnnotDictFromFPDFAnnotation(annot);
+  if (!pAnnotDict) {
+    return false;
+  }
+
+  CPDF_InteractiveForm* pPDFForm = pForm->GetInteractiveForm();
+  CPDF_FormControl* pFormControl = pPDFForm->GetControlByDict(pAnnotDict);
+  if (!pFormControl) {
+    return false;
+  }
+
+  CPDFSDK_Widget* pWidget = pForm->GetWidget(pFormControl);
+  if (!pWidget) {
+    return false;
+  }
+
+  std::optional<FX_COLORREF> text_color = pWidget->GetTextColor();
+  if (!text_color) {
+    return false;
+  }
+
+  *R = FXSYS_GetRValue(*text_color);
+  *G = FXSYS_GetGValue(*text_color);
+  *B = FXSYS_GetBValue(*text_color);
+  return true;
+}
+
 FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FPDFAnnot_IsChecked(FPDF_FORMHANDLE hHandle,
                                                         FPDF_ANNOTATION annot) {
   const CPDFSDK_Widget* pWidget =
