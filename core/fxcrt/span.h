6cb58800368de25fb2052818c924ba07044943b4
Tom Sepez <tsepez@chromium.org>
2024-06-24 19:48:36 +0000

    Add fixed-span versions of first(), last(), and subspan(),
    Similar to base, except:
    
    -- there is a unified template with a size_ member in all cases.
    -- requires clauses are replaced by static asserts.
    -- CHECKS() are intened to short-circuit evaluate to false for
       fixed spand and hopefully be removed by the optimizer.
    
    Also:
    
    -- rename `pos` to `offset` for consistency,
    -- use data() consistently in place of `data_`.
    -- add some missing UNSAFE_BUFFERS() around constructor calls.
    
    Change-Id: I5b7f4c023ac8d56a6961efd2fdf903f505e00af6

--- core/fxcrt/span.h
+++ core/fxcrt/span.h
@@ -19,7 +19,11 @@
 #include "core/fxcrt/unowned_ptr_exclusion.h"
 
 // SAFETY: TODO(crbug.com/pdfium/2085): this entire file is to be replaced
-// with the fully annotated one that is being prepared in base/.
+// with the fully annotated one that is being prepared in Chromium (PDFium
+// is still holding the line at C++17, and Chromium's uses C++20 features).
+
+// This is also simplied from Chromiums as there is always a size_ member,
+// even for fixed spans, to avoid having a second distinct template.
 
 namespace pdfium {
 
@@ -171,9 +175,6 @@ using EnableIfConstSpanCompatibleContainer =
 // - no constructor from a pointer range
 //
 // Differences from [span.sub]:
-// - no templated first()
-// - no templated last()
-// - no templated subspan()
 // - using size_t instead of ptrdiff_t for indexing
 //
 // Differences from [span.obs]:
@@ -265,22 +266,47 @@ class TRIVIAL_ABI GSL_POINTER span {
   ~span() noexcept = default;
 
   // [span.sub], span subviews
+  template <size_t Count>
+  constexpr span<T, Count> first() const noexcept {
+    static_assert(Extent == dynamic_extent || Count <= Extent);
+    CHECK(Extent != dynamic_extent || Count <= size_);
+    return UNSAFE_BUFFERS(span<T, Count>(data(), Count));
+  }
+
   const span first(size_t count) const {
     CHECK(count <= size_);
-    return span(static_cast<T*>(data_), count);
+    return UNSAFE_BUFFERS(span(data(), count));
+  }
+
+  template <size_t Count>
+  constexpr span<T, Count> last() const noexcept {
+    static_assert(Extent == dynamic_extent || Count < Extent);
+    CHECK(Extent != dynamic_extent || Count <= size_);
+    return UNSAFE_BUFFERS(span<T, Count>(data() + (size_ - Count), Count));
   }
 
   const span last(size_t count) const {
     CHECK(count <= size_);
-    return UNSAFE_BUFFERS(
-        span(static_cast<T*>(data_) + (size_ - count), count));
+    return UNSAFE_BUFFERS(span(data() + (size_ - count), count));
   }
 
-  const span subspan(size_t pos, size_t count = dynamic_extent) const {
-    CHECK(pos <= size_);
-    CHECK(count == dynamic_extent || count <= size_ - pos);
-    return span(UNSAFE_BUFFERS(static_cast<T*>(data_) + pos),
-                count == dynamic_extent ? size_ - pos : count);
+  template <size_t Offset, size_t Count = dynamic_extent>
+  constexpr auto subspan() const noexcept {
+    static_assert(Extent == dynamic_extent || Offset <= Extent);
+    static_assert(Extent == dynamic_extent || Count == dynamic_extent ||
+                  Count <= Extent - Offset);
+    CHECK(Extent != dynamic_extent || Offset <= size_);
+    CHECK(Extent != dynamic_extent || Count == dynamic_extent ||
+          Count <= size_ - Offset);
+    return UNSAFE_BUFFERS(span(
+        data() + Offset, Count == dynamic_extent ? size_ - Offset : Count));
+  }
+
+  const span subspan(size_t offset, size_t count = dynamic_extent) const {
+    CHECK(offset <= size_);
+    CHECK(count == dynamic_extent || count <= size_ - offset);
+    return UNSAFE_BUFFERS(span(
+        data() + offset, count == dynamic_extent ? size_ - offset : count));
   }
 
   // [span.obs], span observers
@@ -338,6 +364,10 @@ class TRIVIAL_ABI GSL_POINTER span {
       : data_(data), size_(size) {
     DCHECK(data_ || size_ == 0);
   }
+
+  // Allow all spans to call the above constructor regardless of Extent.
+  friend class span;
+
   template <typename U>
   friend constexpr span<U> make_span(U* data, size_t size) noexcept;
 
