77b00f58d8af95068bbc3783d55adbd94e2b5086
Tom Sepez <tsepez@chromium.org>
2024-07-02 16:22:26 +0000

    Implement Zip<>().
    Take some ideas from C++23's zip_view<> to help improve performance
    of some image manipulations over spans in the subsequent CLs.
    
    Change-Id: Idfb062a8eda40cc11ed614542f2451652c168318

---
+++ core/fxcrt/zip_unittest.cpp
@@ -0,0 +1,50 @@
+// Copyright 2024 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include "core/fxcrt/span.h"
+#include "core/fxcrt/zip.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::AnyOf;
+using ::testing::ElementsAre;
+using ::testing::ElementsAreArray;
+
+// Tests PDFium-modifications to base::span. The name of this file is
+// chosen to avoid collisions with base's span_unittest.cc
+
+namespace fxcrt {
+
+TEST(Zip, EmptyZip) {
+  pdfium::span<const int> nothing;
+  int stuff[] = {1, 2, 3};
+
+  auto zip_nothing_nothing = Zip(nothing, nothing);
+  EXPECT_EQ(zip_nothing_nothing.begin(), zip_nothing_nothing.end());
+
+  auto zip_nothing_stuff = Zip(nothing, stuff);
+  EXPECT_EQ(zip_nothing_stuff.begin(), zip_nothing_stuff.end());
+}
+
+TEST(Zip, ActualZip) {
+  const int stuff[] = {1, 2, 3};
+  const int expected[] = {1, 2, 3, 0};
+  int output[4] = {};
+
+  for (auto item : Zip(stuff, output)) {
+    item.second = item.first;
+  }
+  EXPECT_THAT(output, ElementsAreArray(expected));
+}
+
+TEST(Zip, BadArgumentsZip) {
+  pdfium::span<const int> nothing;
+  int stuff[] = {1, 2, 3};
+
+  EXPECT_DEATH(Zip(stuff, nothing), ".*");
+}
+
+}  // namespace fxcrt
