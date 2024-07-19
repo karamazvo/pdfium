697d8de9f9a7cd1de080132c5f125a5b36252a29
Tom Sepez <tsepez@chromium.org>
2024-07-19 23:23:30 +0000

    Introduce FixedSizeDataVector<>::TruncatedFrom().
    Analogous to base::HeapArray<>::take_from().
    
    This is very similar to the move constructor, except that it gives a
    new vector that claims a smaller size.  When the memory is actually
    freed, the allocator knows the correct size itself from the allocation
    itself despite the smaller claimed size.
    
    This allows the pattern where we guess the size of the vector, write
    some data to it, and then reset the end to match what was written.
    
    In keeping with the idea that vectors should be as immutable as
    possible, require an rvalue-reference as an argument, so that the
    old vector becomse moved-from after creating the new one.
    
    Change-Id: I88656e184ac022af1b4fcfce651f6ff3c7bafb28

--- core/fxcrt/fixed_size_data_vector.h
+++ core/fxcrt/fixed_size_data_vector.h
@@ -10,6 +10,7 @@
 #include <memory>
 #include <utility>
 
+#include "core/fxcrt/check_op.h"
 #include "core/fxcrt/compiler_specific.h"
 #include "core/fxcrt/fx_memory_wrappers.h"
 #include "core/fxcrt/span.h"
@@ -69,6 +70,17 @@ class FixedSizeDataVector {
     // SAFETY: same `size` value passed to FX_TryAlloc() above as
     // passed to ctor when the ptr is non-null.
     return UNSAFE_BUFFERS(FixedSizeDataVector(ptr, ptr ? size : 0u));
+  }
+
+  static FixedSizeDataVector TruncatedFrom(FixedSizeDataVector&& that,
+                                           size_t new_size) {
+    CHECK_LE(new_size, that.size_);
+    that.size_ = 0;
+
+    FixedSizeDataVector result;
+    result.data_ = std::move(that.data_);
+    result.size_ = new_size;
+    return result;
   }
 
   FixedSizeDataVector(const FixedSizeDataVector&) = delete;
