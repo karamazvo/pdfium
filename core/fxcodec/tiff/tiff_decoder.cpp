ebd1e42a6bb665b8b52c83715f1ed715ae8d5fce
Tom Sepez <tsepez@chromium.org>
2024-07-26 22:25:28 +0000

    Prefer reinterpret_span<T> to reinterpret_cast<T*>
    Get a few checks "for free" and avoid explicit mulitply by 4.
    
    Change-Id: I9d4c4201fd76cdb7eea5866dc5d943bbb323a587

--- core/fxcodec/tiff/tiff_decoder.cpp
+++ core/fxcodec/tiff/tiff_decoder.cpp
@@ -433,7 +433,7 @@ bool CTiffContext::Decode(const RetainPtr<CFX_DIBitmap>& pDIBitmap) {
     uint16_t rotation = ORIENTATION_TOPLEFT;
     TIFFGetField(m_tif_ctx.get(), TIFFTAG_ORIENTATION, &rotation);
     uint32_t* data = const_cast<uint32_t*>(
-        reinterpret_cast<const uint32_t*>(pDIBitmap->GetBuffer().data()));
+        fxcrt::reinterpret_span<const uint32_t>(pDIBitmap->GetBuffer()).data());
     if (TIFFReadRGBAImageOriented(m_tif_ctx.get(), img_width, img_height, data,
                                   rotation, 1)) {
       for (uint32_t row = 0; row < img_height; row++) {
