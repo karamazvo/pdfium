793e07579935e91deba96319c8814b4a5016c13f
Lei Zhang <thestig@chromium.org>
2024-08-01 14:30:58 -0700

    Check `FXDIB_Format::kArgbPremul` in CFX_DIBBase::IsPremultiplied()
    Remove `CFX_DIBitmap::m_nFormat` and use `CFX_DIBBase::m_Format` as the
    canonical source of bitmap format information. Also remove
    CFX_DIBitmap::ForcePreMultiply(), as bitmaps can be pre-multiplied to
    start with if they are of `FXDIB_Format::kArgbPremul`.
    
    Update some test expectations to match this change. Most of the changes
    are off by 1 pixel values due to rounding errors. When comparing the
    old and new bitmaps, they are not pixel perfect according to
    pdfium_diff, but will pass with --fuzzy.
    
    Bug: 42271020
    Change-Id: I7726b3915602c0ca1845a19e28f9d2d6cc107faf

--- fpdfsdk/fpdf_progressive.cpp
+++ fpdfsdk/fpdf_progressive.cpp
@@ -68,6 +68,12 @@ FPDF_RenderPageBitmapWithColorScheme_Start(FPDF_BITMAP bitmap,
   CPDF_PageRenderContext* context = owned_context.get();
   pPage->SetRenderContext(std::move(owned_context));
 
+#if defined(PDF_USE_SKIA)
+  if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
+    pBitmap->PreMultiply();
+  }
+#endif
+
   auto device = std::make_unique<CFX_DefaultRenderDevice>();
   device->AttachWithRgbByteOrder(pBitmap, !!(flags & FPDF_REVERSE_BYTE_ORDER));
   context->m_pDevice = std::move(device);
@@ -77,17 +83,27 @@ FPDF_RenderPageBitmapWithColorScheme_Start(FPDF_BITMAP bitmap,
                                 size_y, rotate, flags, color_scheme,
                                 /*need_to_restore=*/false, &pause_adapter);
 
+  if (!context->m_pRenderer) {
+#if defined(PDF_USE_SKIA)
+    if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
+      pBitmap->UnPreMultiply();
+    }
+#endif  // defined(PDF_USE_SKIA)
+
+    return FPDF_RENDER_FAILED;
+  }
+
+  int status = ToFPDFStatus(context->m_pRenderer->GetStatus());
+  if (status == FPDF_RENDER_TOBECONTINUED) {
+    return FPDF_RENDER_TOBECONTINUED;
+  }
+
 #if defined(PDF_USE_SKIA)
   if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
     pBitmap->UnPreMultiply();
   }
 #endif  // defined(PDF_USE_SKIA)
-
-  if (!context->m_pRenderer) {
-    return FPDF_RENDER_FAILED;
-  }
-
-  return ToFPDFStatus(context->m_pRenderer->GetStatus());
+  return status;
 }
 
 FPDF_EXPORT int FPDF_CALLCONV FPDF_RenderPageBitmap_Start(FPDF_BITMAP bitmap,
@@ -121,12 +137,17 @@ FPDF_EXPORT int FPDF_CALLCONV FPDF_RenderPage_Continue(FPDF_PAGE page,
   CPDFSDK_PauseAdapter pause_adapter(pause);
   pContext->m_pRenderer->Continue(&pause_adapter);
 
+  int status = ToFPDFStatus(pContext->m_pRenderer->GetStatus());
+  if (status == FPDF_RENDER_TOBECONTINUED) {
+    return FPDF_RENDER_TOBECONTINUED;
+  }
+
 #if defined(PDF_USE_SKIA)
   if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
     pContext->m_pDevice->GetBitmap()->UnPreMultiply();
   }
 #endif  // defined(PDF_USE_SKIA)
-  return ToFPDFStatus(pContext->m_pRenderer->GetStatus());
+  return status;
 }
 
 FPDF_EXPORT void FPDF_CALLCONV FPDF_RenderPage_Close(FPDF_PAGE page) {
