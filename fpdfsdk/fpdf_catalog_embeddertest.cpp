f26f92d88834acab839d2de6b3201c31ccd2e569
Andy Phan <andyphan@chromium.org>
2024-08-09 13:24:26 -0700

    Add API to set document language in PDF document
    Add experimental API FPDFCatalog_SetLanguage() that allows the user to
    set the /Lang entry in a PDF document's catalog.
    
    Bug: 42270891
    Change-Id: I1ea20bf80f9045bd9fd34e55fcaf58cf82023d3c

---
+++ fpdfsdk/fpdf_catalog_embeddertest.cpp
@@ -0,0 +1,66 @@
+// Copyright 2024 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "public/fpdf_catalog.h"
+
+#include "core/fpdfapi/parser/cpdf_dictionary.h"
+#include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fpdfapi/parser/cpdf_string.h"
+#include "core/fpdfapi/parser/cpdf_test_document.h"
+#include "fpdfsdk/cpdfsdk_helpers.h"
+#include "public/fpdf_edit.h"
+#include "testing/embedder_test.h"
+
+using FPDFCatalogTest = EmbedderTest;
+
+TEST_F(FPDFCatalogTest, SetLanguageInvalidDocument) {
+  // Document cannot be nullptr.
+  EXPECT_FALSE(FPDFCatalog_SetLanguage(nullptr, "en-US"));
+
+  ScopedFPDFDocument doc(FPDF_CreateNewDocument());
+  CPDF_TestDocument* test_document =
+      static_cast<CPDF_TestDocument*>(CPDFDocumentFromFPDFDocument(doc.get()));
+
+  // Catalog cannot be nullptr.
+  test_document->SetRoot(nullptr);
+  EXPECT_FALSE(FPDFCatalog_SetLanguage(doc.get(), "en-US"));
+}
+
+TEST_F(FPDFCatalogTest, SetLanguageNewDocument) {
+  ScopedFPDFDocument doc(FPDF_CreateNewDocument());
+
+  const CPDF_Dictionary* catalog =
+      CPDFDocumentFromFPDFDocument(doc.get())->GetRoot();
+  ASSERT_TRUE(catalog);
+
+  // The new document shouldn't have any entry for /Lang.
+  EXPECT_FALSE(catalog->GetStringFor("Lang"));
+
+  // Add a new entry.
+  EXPECT_TRUE(FPDFCatalog_SetLanguage(doc.get(), "en-US"));
+
+  RetainPtr<const CPDF_String> result_language = catalog->GetStringFor("Lang");
+  ASSERT_TRUE(result_language);
+  EXPECT_EQ("en-US", result_language->GetString());
+}
+
+TEST_F(FPDFCatalogTest, SetLanguageExistingDocument) {
+  ASSERT_TRUE(OpenDocument("tagged_table.pdf"));
+
+  const CPDF_Dictionary* catalog =
+      CPDFDocumentFromFPDFDocument(document())->GetRoot();
+  ASSERT_TRUE(catalog);
+
+  // The PDF already has an existing entry for /Lang.
+  RetainPtr<const CPDF_String> result_language = catalog->GetStringFor("Lang");
+  ASSERT_TRUE(result_language);
+  EXPECT_EQ("en-US", result_language->GetString());
+
+  // Replace the existing entry.
+  EXPECT_TRUE(FPDFCatalog_SetLanguage(document(), "hu"));
+
+  result_language = catalog->GetStringFor("Lang");
+  ASSERT_TRUE(result_language);
+  EXPECT_EQ("hu", result_language->GetString());
+}
