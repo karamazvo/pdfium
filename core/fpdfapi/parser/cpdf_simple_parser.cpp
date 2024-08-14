f403bbb0b3f5a1a48a4466e00bebf7f4637498a4
Andy Phan <andyphan@chromium.org>
2024-08-14 13:39:29 -0700

    Refactor CPDF_SimpleParser part 4
    Avoid an unnecessary delimiter check and extra nesting level when the
    current character is '/'. Move the remaining delimiter logic into a
    separate helper method to make the methods more readable.
    
    Change-Id: Ia31fba726d3b5a0b76c14c5da573315062bfdf10

--- core/fpdfapi/parser/cpdf_simple_parser.cpp
+++ core/fpdfapi/parser/cpdf_simple_parser.cpp
@@ -9,6 +9,7 @@
 #include <cstdint>
 
 #include "core/fpdfapi/parser/fpdf_parser_utility.h"
+#include "core/fxcrt/check_op.h"
 
 CPDF_SimpleParser::CPDF_SimpleParser(pdfium::span<const uint8_t> input)
     : data_(input) {}
@@ -49,74 +50,21 @@ ByteStringView CPDF_SimpleParser::GetWord() {
   }
 
   uint32_t start_pos = cur_pos_ - 1;
-  if (PDFCharIsDelimiter(cur_char)) {
-    // Find names
-    if (cur_char == '/') {
-      while (true) {
-        if (cur_pos_ >= data_.size()) {
-          break;
-        }
-
-        cur_char = data_[cur_pos_++];
-        if (!PDFCharIsOther(cur_char) && !PDFCharIsNumeric(cur_char)) {
-          cur_pos_--;
-          break;
-        }
-      }
-      return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
-    }
-
-    if (cur_char == '<') {
-      if (cur_pos_ >= data_.size()) {
-        return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
-      }
+  // Find names.
+  if (cur_char == '/') {
+    while (cur_pos_ < data_.size()) {
       cur_char = data_[cur_pos_++];
-      if (cur_char != '<') {
-        while (cur_pos_ < data_.size() && data_[cur_pos_] != '>') {
-          cur_pos_++;
-        }
-
-        if (cur_pos_ < data_.size()) {
-          cur_pos_++;
-        }
-      }
-    } else if (cur_char == '>') {
-      if (cur_pos_ >= data_.size()) {
-        return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
-      }
-      cur_char = data_[cur_pos_++];
-      if (cur_char != '>') {
+      if (!PDFCharIsOther(cur_char) && !PDFCharIsNumeric(cur_char)) {
+        // Don't include the current character.
         cur_pos_--;
-      }
-    } else if (cur_char == '(') {
-      int level = 1;
-      while (cur_pos_ < data_.size()) {
-        if (data_[cur_pos_] == ')') {
-          level--;
-          if (level == 0)
-            break;
-        }
-
-        if (data_[cur_pos_] == '\\') {
-          if (cur_pos_ >= data_.size()) {
-            break;
-          }
-
-          cur_pos_++;
-        } else if (data_[cur_pos_] == '(') {
-          level++;
-        }
-        if (cur_pos_ >= data_.size()) {
-          break;
-        }
-
-        cur_pos_++;
-      }
-      if (cur_pos_ < data_.size()) {
-        cur_pos_++;
+        break;
       }
     }
     return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
+  }
+
+  if (PDFCharIsDelimiter(cur_char)) {
+    return HandleDelimiter(cur_char);
   }
 
   while (cur_pos_ < data_.size()) {
@@ -125,6 +73,61 @@ ByteStringView CPDF_SimpleParser::GetWord() {
     if (PDFCharIsDelimiter(cur_char) || PDFCharIsWhitespace(cur_char)) {
       cur_pos_--;
       break;
+    }
+  }
+  return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
+}
+
+ByteStringView CPDF_SimpleParser::HandleDelimiter(uint8_t delimiter) {
+  CHECK_EQ(data_[cur_pos_ - 1], delimiter);
+  uint32_t start_pos = cur_pos_ - 1;
+
+  if (delimiter == '<') {
+    if (cur_pos_ >= data_.size()) {
+      return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
+    }
+    if (data_[cur_pos_++] != '<') {
+      while (cur_pos_ < data_.size() && data_[cur_pos_] != '>') {
+        cur_pos_++;
+      }
+
+      if (cur_pos_ < data_.size()) {
+        cur_pos_++;
+      }
+    }
+  } else if (delimiter == '>') {
+    if (cur_pos_ >= data_.size()) {
+      return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
+    }
+    if (data_[cur_pos_++] != '>') {
+      cur_pos_--;
+    }
+  } else if (delimiter == '(') {
+    int level = 1;
+    while (cur_pos_ < data_.size()) {
+      if (data_[cur_pos_] == ')') {
+        level--;
+        if (level == 0)
+          break;
+      }
+
+      if (data_[cur_pos_] == '\\') {
+        if (cur_pos_ >= data_.size()) {
+          break;
+        }
+
+        cur_pos_++;
+      } else if (data_[cur_pos_] == '(') {
+        level++;
+      }
+      if (cur_pos_ >= data_.size()) {
+        break;
+      }
+
+      cur_pos_++;
+    }
+    if (cur_pos_ < data_.size()) {
+      cur_pos_++;
     }
   }
   return ByteStringView(data_.subspan(start_pos, cur_pos_ - start_pos));
