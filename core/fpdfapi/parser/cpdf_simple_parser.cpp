57ee54bd1f525730a07f587fb3b0781f7b4f5aed
Andy Phan <andyphan@chromium.org>
2024-08-15 10:17:35 -0700

    Refactor CPDF_SimpleParser part 4
    Move the delimiter logic into a separate helper method to make the
    methods more readable.
    
    Change-Id: Ia31fba726d3b5a0b76c14c5da573315062bfdf10

--- core/fpdfapi/parser/cpdf_simple_parser.cpp
+++ core/fpdfapi/parser/cpdf_simple_parser.cpp
@@ -9,6 +9,7 @@
 #include <stdint.h>
 
 #include "core/fpdfapi/parser/fpdf_parser_utility.h"
+#include "core/fxcrt/check_op.h"
 
 CPDF_SimpleParser::CPDF_SimpleParser(pdfium::span<const uint8_t> input)
     : data_(input) {}
@@ -48,84 +49,88 @@ ByteStringView CPDF_SimpleParser::GetWord() {
     }
   }
 
-  uint32_t start_position = cur_position_ - 1;
   if (PDFCharIsDelimiter(cur_char)) {
-    // Find names
-    if (cur_char == '/') {
-      while (cur_position_ < data_.size()) {
-        cur_char = data_[cur_position_];
-        // Don't include characters that aren't other nor numeric.
-        if (!PDFCharIsOther(cur_char) && !PDFCharIsNumeric(cur_char)) {
-          return ByteStringView(
-              data_.subspan(start_position, cur_position_ - start_position));
-        }
-        ++cur_position_;
-      }
-      return ByteStringView();
-    }
-
-    if (cur_char == '<') {
-      if (cur_position_ >= data_.size()) {
-        return ByteStringView(
-            data_.subspan(start_position, cur_position_ - start_position));
-      }
-      cur_char = data_[cur_position_++];
-      if (cur_char != '<') {
-        while (cur_position_ < data_.size() && data_[cur_position_] != '>') {
-          cur_position_++;
-        }
-
-        if (cur_position_ < data_.size()) {
-          cur_position_++;
-        }
-      }
-    } else if (cur_char == '>') {
-      if (cur_position_ >= data_.size()) {
-        return ByteStringView(
-            data_.subspan(start_position, cur_position_ - start_position));
-      }
-      cur_char = data_[cur_position_++];
-      if (cur_char != '>') {
-        cur_position_--;
-      }
-    } else if (cur_char == '(') {
-      int level = 1;
-      while (cur_position_ < data_.size()) {
-        if (data_[cur_position_] == ')') {
-          level--;
-          if (level == 0)
-            break;
-        }
-
-        if (data_[cur_position_] == '\\') {
-          if (cur_position_ >= data_.size()) {
-            break;
-          }
-
-          cur_position_++;
-        } else if (data_[cur_position_] == '(') {
-          level++;
-        }
-        if (cur_position_ >= data_.size()) {
-          break;
-        }
-
-        cur_position_++;
-      }
-      if (cur_position_ < data_.size()) {
-        cur_position_++;
-      }
-    }
-    return ByteStringView(
-        data_.subspan(start_position, cur_position_ - start_position));
+    return HandleDelimiter(cur_char);
   }
 
+  uint32_t start_position = cur_position_ - 1;
   while (cur_position_ < data_.size()) {
     cur_char = data_[cur_position_];
     if (PDFCharIsDelimiter(cur_char) || PDFCharIsWhitespace(cur_char)) {
       break;
     }
     ++cur_position_;
+  }
+  return ByteStringView(
+      data_.subspan(start_position, cur_position_ - start_position));
+}
+
+ByteStringView CPDF_SimpleParser::HandleDelimiter(uint8_t delimiter) {
+  CHECK_EQ(data_[cur_position_ - 1], delimiter);
+  uint32_t start_position = cur_position_ - 1;
+
+  if (delimiter == '/') {
+    // Find names.
+    while (cur_position_ < data_.size()) {
+      uint8_t cur_char = data_[cur_position_];
+      // Don't include characters that aren't other nor numeric.
+      if (!PDFCharIsOther(cur_char) && !PDFCharIsNumeric(cur_char)) {
+        return ByteStringView(
+            data_.subspan(start_position, cur_position_ - start_position));
+      }
+      ++cur_position_;
+    }
+    return ByteStringView();
+  } else if (delimiter == '<') {
+    if (cur_position_ >= data_.size()) {
+      return ByteStringView(
+          data_.subspan(start_position, cur_position_ - start_position));
+    }
+    if (data_[cur_position_++] != '<') {
+      while (cur_position_ < data_.size() && data_[cur_position_] != '>') {
+        cur_position_++;
+      }
+
+      if (cur_position_ < data_.size()) {
+        cur_position_++;
+      }
+    }
+  } else if (delimiter == '>') {
+    if (cur_position_ >= data_.size()) {
+      return ByteStringView(
+          data_.subspan(start_position, cur_position_ - start_position));
+    }
+    if (data_[cur_position_++] != '>') {
+      cur_position_--;
+    }
+  } else if (delimiter == '(') {
+    int level = 1;
+    while (cur_position_ < data_.size()) {
+      if (data_[cur_position_] == ')') {
+        level--;
+        if (level == 0) {
+          break;
+        }
+      }
+
+      if (data_[cur_position_] == '\\') {
+        if (cur_position_ >= data_.size()) {
+          break;
+        }
+
+        cur_position_++;
+      } else if (data_[cur_position_] == '(') {
+        level++;
+      }
+      if (cur_position_ >= data_.size()) {
+        break;
+      }
+
+      cur_position_++;
+    }
+    if (cur_position_ < data_.size()) {
+      cur_position_++;
+    }
   }
   return ByteStringView(
       data_.subspan(start_position, cur_position_ - start_position));
