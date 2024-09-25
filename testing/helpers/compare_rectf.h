be2286aa7c701b94dae19c988bfa4f3f455b5df7
Saiv-kodes <satvicdhawan14@gmail.com>
2024-09-25 21:12:38 +0530

    Creating a util function to compare two FS_RECTF objects
    This util function aims to ease the process of comparing two FS_RECTF
    objects.
    The process is currently carried out through a chain of ASSERT_EQ
    function calls for each of the fields(top,left,bottom,right) of the
    FS_RECTF struct.
    
    Bug: 42270440
    Change-Id: Ied7467758817efa8f9a1ce0e5f767a462918cb22

---
+++ testing/helpers/compare_rectf.h
@@ -0,0 +1,12 @@
+// Copyright 2024 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TESTING_HELPERS_COMPARE_RECTF_H
+#define TESTING_HELPERS_COMPARE_RECTF_H
+
+#include "public/fpdfview.h"
+
+bool Compare_FSRECTF(FS_RECTF& model, FS_RECTF& subject);
+
+#endif  // TESTING_HELPERS_COMPARE_RECTF_H
