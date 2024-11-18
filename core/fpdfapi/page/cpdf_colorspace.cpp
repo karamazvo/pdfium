a5af3c1b0cd4486c2b15be3c05cd383006427e8f
Haoran Tang <haoran.tang.personal@gmail.com>
2024-11-17 21:41:31 -0600

    WIP: Add support for ICC profile extraction
    Introduces functionality to extract ICC profile metadata associated
    with images embedded in PDF documents. Add new api to access key details
    about the ICC profile.
    
    Bug: 1468
    Change-Id: I1bca935dc19cb24df990455e0ff1963a737403dd

--- core/fpdfapi/page/cpdf_colorspace.cpp
+++ core/fpdfapi/page/cpdf_colorspace.cpp
@@ -217,6 +217,7 @@ class CPDF_ICCBasedCS final : public CPDF_BasedCS {
   // CPDF_ColorSpace:
   std::optional<FX_RGB_STRUCT<float>> GetRGB(
       pdfium::span<const float> pBuf) const override;
+  RetainPtr<CPDF_IccProfile> GetIccProfile() const override;
   void TranslateImageLine(pdfium::span<uint8_t> dest_span,
                           pdfium::span<const uint8_t> src_span,
                           int pixels,
@@ -952,6 +953,10 @@ std::optional<FX_RGB_STRUCT<float>> CPDF_ICCBasedCS::GetRGB(
     return m_pBaseCS->GetRGB(pBuf);
   }
   return FX_RGB_STRUCT<float>{};
+}
+
+RetainPtr<CPDF_IccProfile> GetIccProfile() const {
+  return profile_;
 }
 
 void CPDF_ICCBasedCS::TranslateImageLine(pdfium::span<uint8_t> dest_span,
