ee0e4aeb792ad331c6da14a8c88071bbef9c9c1f
Tom Sepez <tsepez@chromium.org>
2024-11-22 19:00:45 +0000

    Tidy JpegProgressiveDecoder::ReadHeader() return results.
    Then re-write caller to be slightly saner.
    
    Change-Id: Icc7214ce027fbd8cf30470cad206ed79b4c6eae7

--- core/fxcodec/jpeg/jpeg_progressive_decoder.cpp
+++ core/fxcodec/jpeg/jpeg_progressive_decoder.cpp
@@ -115,19 +115,19 @@ int JpegProgressiveDecoder::ReadHeader(Context* pContext,
   auto* ctx = static_cast<CJpegContext*>(pContext);
   int ret = jpeg_common_read_header(&ctx->m_Common, TRUE);
   if (ret == -1) {
-    return -1;
+    return kFatal;
   }
   if (ret == JPEG_SUSPENDED) {
-    return 2;
+    return kNeedsMoreInput;
   }
   if (ret != JPEG_HEADER_OK) {
-    return 1;
+    return kError;
   }
   *width = ctx->m_Common.cinfo.image_width;
   *height = ctx->m_Common.cinfo.image_height;
   *nComps = ctx->m_Common.cinfo.num_components;
   JpegLoadAttribute(ctx->m_Common.cinfo, pAttribute);
-  return 0;
+  return kOk;
 }
 
 // static
