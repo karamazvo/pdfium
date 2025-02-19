dd23425f2d53653dffd05c2d4f79161cb1d67b1e
Lei Zhang <thestig@chromium.org>
2025-02-18 18:03:29 -0800

    Consolidate pre-multiply checks into a single function
    Take a bunch of IsPremultiplied() checks and consolidate them into
    ValidateBitmapPremultiplyState(). With a single function, it will be
    easier to change the logic later.
    
    Bug: 42271033
    Change-Id: I8d853a96f3f9ce122e786b4012e29878f4deaef6

--- fpdfsdk/cpdfsdk_helpers.h
+++ fpdfsdk/cpdfsdk_helpers.h
@@ -264,6 +264,10 @@ inline XObjectContext* XObjectContextFromFPDFXObject(FPDF_XOBJECT xobject) {
 
 FXDIB_Format FXDIBFormatFromFPDFFormat(int format);
 
+// Make sure `bitmap` is not pre-multiplied, as PDFium does not take or handle
+// out pre-multiplied bitmaps to the embedder.
+void ValidateBitmapPremultiplyState(CFX_DIBitmap* bitmap);
+
 CPDFSDK_InteractiveForm* FormHandleToInteractiveForm(FPDF_FORMHANDLE hHandle);
 
 UNSAFE_BUFFER_USAGE ByteString
