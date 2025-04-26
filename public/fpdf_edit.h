b781af356b2de293c452c78812847bc2c4bb66ab
Helmut Januschka <helmut@januschka.com>
2025-04-26 19:19:45 +0200

    Add FPDFPage_InsertObjectAtIndex() API
    Adds a new API to insert page objects at a specific index position in a
    page's object list. This provides more control over object ordering
    compared to the existing FPDFPage_InsertObject() which only appends
    objects.
    
    Bug: 389726697
    Change-Id: I8f48a629c41234bbde2f62cafa341efbf140f541

--- public/fpdf_edit.h
+++ public/fpdf_edit.h
@@ -187,6 +187,22 @@ FPDF_EXPORT void FPDF_CALLCONV FPDFPage_SetRotation(FPDF_PAGE page, int rotate);
 FPDF_EXPORT void FPDF_CALLCONV
 FPDFPage_InsertObject(FPDF_PAGE page, FPDF_PAGEOBJECT page_object);
 
+// Insert |page_object| into |page| at the specified |index|.
+//
+//   page        - handle to a page
+//   page_object - handle to a page object. The |page_object| will be
+//                 automatically freed.
+//   index       - the index position to insert the object at. If index equals
+//                 the current object count, the object will be appended to the
+//                 end. If index is greater than the object count, the function
+//                 will fail and return false.
+//
+// Returns true if successful.
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFPage_InsertObjectAtIndex(FPDF_PAGE page,
+                             FPDF_PAGEOBJECT page_object,
+                             size_t index);
+
 // Experimental API.
 // Remove |page_object| from |page|.
 //
