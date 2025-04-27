6e8689cfeacb6cf8e7508e9acc222d2ae89b8401
Nico Weber <thakis@chromium.org>
2025-04-27 13:45:26 -0400

    Interpolate shadings in correct color space
    ...and evaluate functions per-pixel.
    (If this is a performance issue, build an 1-d LUT and
    get colors from there, similar to GetShadingSteps().)
    
    This fixes two bugs:
    
    1. We interpolated gouraud and patch colors in RGB,
       instead of in the shading's color space as dictated
       by the spec. (Especially visible for e.g. LAB color
       spaces.) Now we interpolate in the right color space
       and only convert to RGB when painting a primitive.
       (There's no bug filed for this yet.)
    
    2. We evaluated functions too early, causing us to miss
       function values. Now we evaluate functions only when
       painting a primitive. (If this becomes a performance
       problem, we could precompute an 1D LUT using
       GetShadingSteps(), which is used by the radial and
       axial shadings.)
    
    (*: It looks like arrays of 1-element functions never
    worked in PDFium; this CL currently doesn't change that.)
    
    CPDF_MeshStream::ReadColor() no longer returns an
    RGB value but all components. The rest is mechanical
    fallout from this.
    
    Bug: 406662638,42270551,413597066
    Change-Id: I05616987674da10db90deb73ae75a95688d264f8

--- core/fpdfapi/page/cpdf_meshstream.cpp
+++ core/fpdfapi/page/cpdf_meshstream.cpp
@@ -133,7 +133,7 @@ bool CPDF_MeshStream::Load() {
   }
 
   uint32_t nComponents = cs_->ComponentCount();
-  if (nComponents > kMaxComponents) {
+  if (nComponents > kMaxMeshColorComponents) {
     return false;
   }
 
@@ -205,25 +205,17 @@ CFX_PointF CPDF_MeshStream::ReadCoords() const {
   return pos;
 }
 
-FX_RGB_STRUCT<float> CPDF_MeshStream::ReadColor() const {
+CPDF_MeshColor CPDF_MeshStream::ReadColor() const {
   DCHECK(ShouldCheckBPC(type_));
 
-  std::array<float, kMaxComponents> color_value;
+  CPDF_MeshColor color_value;
   for (uint32_t i = 0; i < components_; ++i) {
     color_value[i] = color_min_[i] + bit_stream_->GetBits(component_bits_) *
                                          (color_max_[i] - color_min_[i]) /
                                          component_max_;
   }
-  if (funcs_.empty()) {
-    return cs_->GetRGBOrZerosOnError(color_value);
-  }
-  float result[kMaxComponents] = {};
-  for (const auto& func : funcs_) {
-    if (func && func->OutputCount() <= kMaxComponents) {
-      func->Call(pdfium::make_span(color_value).first<1u>(), result);
-    }
-  }
-  return cs_->GetRGBOrZerosOnError(result);
+
+  return color_value;
 }
 
 bool CPDF_MeshStream::ReadVertex(const CFX_Matrix& pObject2Bitmap,
