a4a63727468c93751873a3d18dc5f9980f3d0ba1
Helmut Januschka <helmut@januschka.com>
2025-05-08 22:32:07 +0200

    Add FPDFFormObj_RemoveObject API to remove objects from forms
    This CL adds a new API function to allow removing page objects from form
    objects. Previously, it was not possible to remove specific page objects
    (like images) that were part of a Form XObject. Now,
    FPDFFormObj_RemoveObject can be used to remove objects from form objects
    similar to how FPDFPage_RemoveObject works for objects directly on a
    page.
    
    Bug: 42271087
    Change-Id: I08e647b91cefb786d71ee846e3f00c8883fd25db

--- fpdfsdk/fpdf_editpage.cpp
+++ fpdfsdk/fpdf_editpage.cpp
@@ -155,6 +155,17 @@ const CPDF_PageObjectHolder* CPDFPageObjHolderFromFPDFFormObject(
   return pFormObject ? pFormObject->form() : nullptr;
 }
 
+// Non-const version needed for FPDFFormObj_RemoveObject
+CPDF_PageObjectHolder* MutableCPDFPageObjHolderFromFPDFFormObject(
+    FPDF_PAGEOBJECT page_object) {
+  CPDF_FormObject* pFormObject = CPDFFormObjectFromFPDFPageObject(page_object);
+  if (!pFormObject) {
+    return nullptr;
+  }
+
+  return pFormObject->form();
+}
+
 }  // namespace
 
 FPDF_EXPORT FPDF_DOCUMENT FPDF_CALLCONV FPDF_CreateNewDocument() {
@@ -1156,4 +1167,21 @@ FPDFFormObj_GetObject(FPDF_PAGEOBJECT form_object, unsigned long index) {
 
   return FPDFPageObjectFromCPDFPageObject(
       pObjectList->GetPageObjectByIndex(index));
+}
+
+FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV
+FPDFFormObj_RemoveObject(FPDF_PAGEOBJECT form_object,
+                         FPDF_PAGEOBJECT page_object) {
+  CPDF_PageObject* cpage_object = CPDFPageObjectFromFPDFPageObject(page_object);
+  if (!cpage_object) {
+    return false;
+  }
+
+  auto* object_holder = MutableCPDFPageObjHolderFromFPDFFormObject(form_object);
+  if (!object_holder) {
+    return false;
+  }
+
+  // Caller takes ownership of the removed page object
+  return !!object_holder->RemovePageObject(cpage_object).release();
 }
