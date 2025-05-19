5dd7991d491cbc4ade9800dde23bd3cfc6fab2d3
djrecipe <brkfstmnchr@gmail.com>
2025-05-19 17:18:21 +0700

    remove form entries when flattening
    - updated `FPDFPage_Flatten` to also remove acroform annots
    - add test `FPDFFlattenEmbedderTest::Bug349502141`
    
    Bug: 349502141
    Change-Id: I1314d679b89221f4a4a758542b1f5a3a37d34bdb

--- fpdfsdk/fpdf_flatten.cpp
+++ fpdfsdk/fpdf_flatten.cpp
@@ -95,6 +95,33 @@ void ParserStream(const CPDF_Dictionary* pPageDic,
   pObjectArray->push_back(pStream);
 }
 
+// recursively searches the form field dictionary for specified annotation
+// (returns true when removed)
+bool RemoveAnnotationFromFormField(RetainPtr<CPDF_Dictionary> dict,
+                                   uint32_t obj_num) {
+  if (!dict) {
+    return false;
+  }
+  RetainPtr<CPDF_Array> array = dict->GetMutableArrayFor("Kids");
+  if (!array) {
+    return false;
+  }
+  for (size_t i = 0; i < array->size(); i++) {
+    RetainPtr<CPDF_Object> obj = array->GetMutableDirectObjectAt(i);
+    if (!obj) {
+      continue;
+    }
+    if (obj->GetObjNum() == obj_num) {
+      array->RemoveAt(i);
+      return true;
+    }
+    if (RemoveAnnotationFromFormField(obj->GetMutableDict(), obj_num)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int ParserAnnots(CPDF_Document* pSourceDoc,
                  RetainPtr<CPDF_Dictionary> pPageDic,
                  std::vector<CFX_FloatRect>* pRectArray,
@@ -102,6 +129,16 @@ int ParserAnnots(CPDF_Document* pSourceDoc,
                  int nUsage) {
   if (!pSourceDoc) {
     return FLATTEN_FAIL;
+  }
+
+  // get acroforms dict
+  CPDF_Dictionary* root = pSourceDoc->GetMutableRoot();
+  RetainPtr<CPDF_Array> fields;
+  if (root) {
+    auto acro_dict = root->GetMutableDictFor("AcroForm");
+    if (acro_dict) {
+      fields = acro_dict->GetMutableArrayFor("Fields");
+    }
   }
 
   GetContentsRect(pSourceDoc, pPageDic, pRectArray);
@@ -137,6 +174,20 @@ int ParserAnnots(CPDF_Document* pSourceDoc,
     }
     if (bParseStream) {
       ParserStream(pPageDic.Get(), pAnnotDict.Get(), pRectArray, pObjectArray);
+    }
+
+    if (fields) {
+      for (size_t i = 0; i < fields->size(); i++) {
+        RetainPtr<CPDF_Object> obj = fields->GetMutableDirectObjectAt(i);
+        if (obj->GetObjNum() == pAnnotDict->GetObjNum()) {
+          fields->RemoveAt(i);
+          break;
+        }
+        if (RemoveAnnotationFromFormField(pAnnotDict,
+                                          pAnnotDict->GetObjNum())) {
+          break;
+        }
+      }
     }
   }
   return FLATTEN_SUCCESS;
