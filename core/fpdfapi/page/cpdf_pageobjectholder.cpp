93a61014f9fa58ba8b9f36e9a1faaa2844eff56c
Helmut Januschka <helmut@januschka.com>
2025-05-31 18:12:08 +0200

    Add FPDFPage_InsertObjectAtIndex() API
    Adds a new API to insert page objects at a specific index position in a
    page's object list. This provides more control over object ordering
    compared to the existing FPDFPage_InsertObject() which only appends
    objects.
    
    Bug: 389726697
    Change-Id: I8f48a629c41234bbde2f62cafa341efbf140f541

--- core/fpdfapi/page/cpdf_pageobjectholder.cpp
+++ core/fpdfapi/page/cpdf_pageobjectholder.cpp
@@ -188,6 +188,19 @@ void CPDF_PageObjectHolder::AppendPageObject(
   page_object_list_.push_back(std::move(pPageObj));
 }
 
+bool CPDF_PageObjectHolder::InsertPageObjectAtIndex(
+    size_t index,
+    std::unique_ptr<CPDF_PageObject> page_obj) {
+  CHECK(page_obj);
+  if (index > page_object_list_.size()) {
+    return false;
+  }
+
+  page_object_list_.insert(UNSAFE_TODO(page_object_list_.begin() + index),
+                           std::move(page_obj));
+  return true;
+}
+
 std::unique_ptr<CPDF_PageObject> CPDF_PageObjectHolder::RemovePageObject(
     CPDF_PageObject* pPageObj) {
   auto it =
@@ -213,6 +226,6 @@ bool CPDF_PageObjectHolder::ErasePageObjectAtIndex(size_t index) {
     return false;
   }
 
-  page_object_list_.erase(page_object_list_.begin() + index);
+  page_object_list_.erase(UNSAFE_TODO(page_object_list_.begin() + index));
   return true;
 }
