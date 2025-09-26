5a8002f0d4a22c58959b48d0ecf9ddbe2bcbc146
Lukasz Anforowicz <lukasza@google.com>
2025-09-26 18:43:12 +0000

    [rust png] Add support for using Skia's Rust-based PNG decoder.
    This CL:
    
    * If `pdf_use_skia` is set in `args.gn` then `libpng`-based decoder with
      one based on Skia.
    * Under the covers the Skia-based PNG decoder uses either:
        - `libpng` (the default)
        - or Rust `png` crate (when `pdf_enable_rust_png` is set in `args.gn`).
    
    Bug: chromium:444045690
    Change-Id: Ia6ec12d0335b1fa74c278acc94973e97aaa30b60

---
+++ core/fxcodec/png/skia_png_decoder.h
@@ -0,0 +1,43 @@
+// Copyright 2025 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
+
+#ifndef CORE_FXCODEC_PNG_SKIA_PNG_DECODER_H_
+#define CORE_FXCODEC_PNG_SKIA_PNG_DECODER_H_
+
+#include <memory>
+
+#include "core/fxcodec/progressive_decoder_iface.h"
+#include "core/fxcrt/retain_ptr.h"
+
+#ifndef PDF_ENABLE_XFA_PNG
+#error "PNG must be enabled"
+#endif
+
+namespace fxcodec {
+
+class PngDecoderDelegate;
+
+// PNG decoder that uses the Skia library to decode pixels.  (Whether
+// `SkPngDecoder` vs `SkPngRustDecoder` is used depends on whether the build
+// configuration sets certain macro definitions like `PDF_ENABLE_RUST_PNG`).
+class SkiaPngDecoder {
+ public:
+  static std::unique_ptr<ProgressiveDecoderIface::Context> StartDecode(
+      PngDecoderDelegate* pDelegate);
+
+  static bool ContinueDecode(ProgressiveDecoderIface::Context* pContext,
+                             RetainPtr<CFX_CodecMemory> codec_memory);
+
+  SkiaPngDecoder() = delete;
+  SkiaPngDecoder(const SkiaPngDecoder&) = delete;
+  SkiaPngDecoder& operator=(const SkiaPngDecoder&) = delete;
+};
+
+}  // namespace fxcodec
+
+using PngDecoder = fxcodec::SkiaPngDecoder;
+
+#endif  // CORE_FXCODEC_PNG_SKIA_PNG_DECODER_H_
