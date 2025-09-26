2e18b648672a8962503efcc0aeb6804ea6127003
Lukasz Anforowicz <lukasza@google.com>
2025-09-26 18:50:38 +0000

    [rust png] DO NOT SUBMIT - smoke test of Rust PNG mode for PDFium.
    Bug: None / DO NOT SUBMIT
    Change-Id: I14eb69f188b925dabc094fc16d699cec675a4b38

--- core/fxcodec/png/skia_png_decoder.h
+++ core/fxcodec/png/skia_png_decoder.h
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
-
 #ifndef CORE_FXCODEC_PNG_SKIA_PNG_DECODER_H_
 #define CORE_FXCODEC_PNG_SKIA_PNG_DECODER_H_
 
