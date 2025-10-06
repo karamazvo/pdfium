285afc9efe50314ec624b1dca5cb8ee6e8b366b0
Lukasz Anforowicz <lukasza@google.com>
2025-10-06 16:45:39 +0000

    [rust png] Use Rust-based PNG codec from PDFium tests.
    This CL:
    
    * Adds `pdf_enable_rust_png` GN arg
    * When the arg is true, then uses Rust-based PNG codec (instead of
      `libpng`-based one) from `pdfium/testing/helpers/write.cc` and
      from `testing/image_diff/image_diff_png...`
    
    Deferred to future CLs:
    
    * Switching the product code underneath `core/fxcodec/png/...`
      (see https://pdfium-review.googlesource.com/c/pdfium/+/135730)
    * Configuring Chromium to use the new build mode
      (Note that no Chromium tests have been run yet against the new build
      mode of PDFium.)
    
    The new code from this CL is *mostly* covered by the `linux_skia_rust`
    bot on CQ.  The following ad-hoc follow-up CLs break the new code from
    this CL in `testing/image_diff/image_diff_png_skia.cpp` to show/verify
    that this would have caused CQ failures:
    
    * https://pdfium-review.googlesource.com/136490 - coverage of encoding
    * https://pdfium-review.googlesource.com/136510 - coverage of decoding
    
    OTOH note that encoding path in `testing/helpers/write.cc` is *not*
    covered by CQ (before and after this CL, so this shouldn't block landing
    this CL).  In particular, the following CLs pass CQ:
    
    * https://pdfium-review.googlesource.com/136111
    * https://pdfium-review.googlesource.com/136511
    
    Bug: chromium:444045690
    Change-Id: Ica54e6e82d5646267242fd0a6d6538389bd18deb

---
+++ testing/image_diff/image_diff_png_skia.cpp
@@ -0,0 +1,187 @@
+// Copyright 2025 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/image_diff/image_diff_png.h"
+
+#include <algorithm>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "core/fxcrt/check_op.h"
+#include "core/fxcrt/span.h"
+#include "third_party/skia/include/core/SkColorSpace.h"
+#include "third_party/skia/include/core/SkColorType.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
+#include "third_party/skia/include/core/SkStream.h"
+
+#ifdef PDF_ENABLE_RUST_PNG
+#include "third_party/skia/include/codec/SkPngRustDecoder.h"
+#include "third_party/skia/include/encode/SkPngRustEncoder.h"
+#else
+#include "third_party/skia/include/codec/SkPngDecoder.h"
+#include "third_party/skia/include/encode/SkPngEncoder.h"
+#endif
+
+namespace image_diff_png {
+
+namespace {
+
+std::vector<uint8_t> EncodePNG(pdfium::span<const uint8_t> input,
+                               SkColorType color,
+                               SkAlphaType alpha,
+                               size_t width,
+                               size_t height,
+                               size_t row_byte_width) {
+  CHECK_LE(row_byte_width * height, input.size());
+
+  SkImageInfo info = SkImageInfo::Make(pdfium::checked_cast<int>(width),
+                                       pdfium::checked_cast<int>(height), color,
+                                       alpha, SkColorSpace::MakeSRGB());
+
+  SkPixmap pixmap(info, input.data(), row_byte_width);
+#ifdef PDF_ENABLE_RUST_PNG
+  sk_sp<SkData> data = SkPngRustEncoder::Encode(pixmap, {});
+#else
+  sk_sp<SkData> data = SkPngEncoder::Encode(pixmap, {});
+#endif
+  SkSpan<const uint8_t> span = data->byteSpan();
+  return std::vector<uint8_t>(span.begin(), span.end());
+}
+
+}  // namespace
+
+std::vector<uint8_t> DecodePNG(pdfium::span<const uint8_t> input,
+                               bool reverse_byte_order,
+                               int* width,
+                               int* height) {
+  CHECK(width);
+  CHECK(height);
+
+  auto stream = std::make_unique<SkMemoryStream>(input.data(), input.size(),
+                                                 /*copyData=*/false);
+#ifdef PDF_ENABLE_RUST_PNG
+  std::unique_ptr<SkCodec> codec =
+      SkPngRustDecoder::Decode(std::move(stream), nullptr);
+#else
+  std::unique_ptr<SkCodec> codec =
+      SkPngDecoder::Decode(std::move(stream), nullptr);
+#endif
+  if (!codec) {
+    return {};
+  }
+
+  SkColorType format =
+      reverse_byte_order ? kBGRA_8888_SkColorType : kRGBA_8888_SkColorType;
+  SkImageInfo info = codec->getInfo();
+  info = info.makeColorType(format);
+  info = info.makeColorSpace(SkColorSpace::MakeSRGB());
+
+  *width = info.width();
+  *height = info.height();
+
+  std::vector<uint8_t> output;
+  output.resize(info.computeMinByteSize());
+
+  SkCodec::Result result =
+      codec->getPixels(info, output.data(), info.minRowBytes());
+  if (result != SkCodec::kSuccess) {
+    return {};
+  }
+  return output;
+}
+
+// Encode a BGR pixel array into a PNG.
+std::vector<uint8_t> EncodeBGRPNG(pdfium::span<const uint8_t> bgr_input,
+                                  int int_width,
+                                  int int_height,
+                                  int int_row_byte_width) {
+  size_t width = pdfium::checked_cast<size_t>(int_width);
+  size_t height = pdfium::checked_cast<size_t>(int_height);
+  size_t row_byte_width = pdfium::checked_cast<size_t>(int_row_byte_width);
+
+  // Crude integer overflow protection.  All test images are expected to be
+  // smaller than this.
+  CHECK_LE(width, 10000u);
+  CHECK_LE(height, 10000u);
+
+  CHECK_LE(width * 3, row_byte_width);
+  CHECK_LE(row_byte_width * height, bgr_input.size());
+
+  std::vector<uint8_t> intermediate_bgra_buf;
+  size_t intermediate_bgra_stride = width * 4;
+  {
+    intermediate_bgra_buf.resize(intermediate_bgra_stride * height);
+    pdfium::span<const uint8_t> src = bgr_input;
+    pdfium::span<uint8_t> dst = intermediate_bgra_buf;
+    for (size_t y = 0; y < height; y++) {
+      for (size_t x = 0; x < width; x++) {
+        dst[x * 4 + 0] = src[x * 3 + 0];
+        dst[x * 4 + 1] = src[x * 3 + 1];
+        dst[x * 4 + 2] = src[x * 3 + 2];
+        dst[x * 4 + 3] = 0xFF;  // opaque
+      }
+      src = src.subspan(std::min<size_t>(row_byte_width, src.size()));
+      dst = dst.subspan(std::min(intermediate_bgra_stride, dst.size()));
+    }
+  }
+
+  return EncodePNG(intermediate_bgra_buf, kBGRA_8888_SkColorType,
+                   kOpaque_SkAlphaType, width, height,
+                   intermediate_bgra_stride);
+}
+
+// Encode an RGBA pixel array into a PNG.
+std::vector<uint8_t> EncodeRGBAPNG(pdfium::span<const uint8_t> input,
+                                   int width,
+                                   int height,
+                                   int row_byte_width) {
+  // Crude integer overflow protection.  All test images are expected to be
+  // smaller than this.
+  CHECK_LE(width, 10000);
+  CHECK_LE(height, 10000);
+
+  CHECK_LE(width * 4, row_byte_width);
+  return EncodePNG(input, kRGBA_8888_SkColorType, kUnpremul_SkAlphaType,
+                   pdfium::checked_cast<size_t>(width),
+                   pdfium::checked_cast<size_t>(height),
+                   pdfium::checked_cast<size_t>(row_byte_width));
+}
+
+// Encode an BGRA pixel array into a PNG.
+std::vector<uint8_t> EncodeBGRAPNG(pdfium::span<const uint8_t> input,
+                                   int width,
+                                   int height,
+                                   int row_byte_width,
+                                   bool discard_transparency) {
+  // Crude integer overflow protection.  All test images are expected to be
+  // smaller than this.
+  CHECK_LE(width, 10000);
+  CHECK_LE(height, 10000);
+
+  CHECK_LE(width * 4, row_byte_width);
+  return EncodePNG(input, kBGRA_8888_SkColorType, kUnpremul_SkAlphaType,
+                   pdfium::checked_cast<size_t>(width),
+                   pdfium::checked_cast<size_t>(height),
+                   pdfium::checked_cast<size_t>(row_byte_width));
+}
+
+// Encode a grayscale pixel array into a PNG.
+std::vector<uint8_t> EncodeGrayPNG(pdfium::span<const uint8_t> input,
+                                   int width,
+                                   int height,
+                                   int row_byte_width) {
+  // Crude integer overflow protection.  All test images are expected to be
+  // smaller than this.
+  CHECK_LE(width, 10000);
+  CHECK_LE(height, 10000);
+
+  CHECK_LE(width * 1, row_byte_width);
+  return EncodePNG(input, kGray_8_SkColorType, kOpaque_SkAlphaType,
+                   pdfium::checked_cast<size_t>(width),
+                   pdfium::checked_cast<size_t>(height),
+                   pdfium::checked_cast<size_t>(row_byte_width));
+}
+
+}  // namespace image_diff_png
