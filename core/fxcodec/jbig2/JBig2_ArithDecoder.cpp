b1285b8454b2034c702daaf819d90c367d98a3ea
Nico Weber <thakis@chromium.org>
2025-10-08 19:20:03 -0400

    undo incorrect arithmetic decoder change + follow-ups
    revert https://pdfium-review.googlesource.com/c/pdfium/+/52712
    revert https://pdfium-review.googlesource.com/c/pdfium/+/16791
    revert https://pdfium-review.googlesource.com/c/pdfium/+/18333
    revert non-stylistic parts of https://pdfium-review.googlesource.com/c/pdfium/+/3244
    
    all no test :(
    
    XXX describe test
    
    XXX say it also fixes 042_25.jb2
    
    XXX fuzz fix
    
    XXX check that the three fuzzer test cases don't time out,
    check test from 41450095 still works
    
    Bug: 432920906,41450095,40548819,40540933,40493521
    Change-Id: Idc5c13c9440d0b4749f0832659bcdf36c1bc3597

--- core/fxcodec/jbig2/JBig2_ArithDecoder.cpp
+++ core/fxcodec/jbig2/JBig2_ArithDecoder.cpp
@@ -94,22 +94,6 @@ void CJBig2_ArithDecoder::BYTEIN() {
     unsigned char B1 = stream_->getNextByte_arith();
     if (B1 > 0x8f) {
       ct_ = 8;
-
-      switch (state_) {
-        case StreamState::kDataAvailable:
-          // Finished decoding data (see JBIG2 spec, Section E.3.4).
-          state_ = StreamState::kDecodingFinished;
-          break;
-        case StreamState::kDecodingFinished:
-          // Allow one more call in the finished state. https://crbug.com/947622
-          state_ = StreamState::kLooping;
-          break;
-        case StreamState::kLooping:
-          // Looping state detected. Mark decoding as complete to bail out.
-          // https://crbug.com/767156
-          complete_ = true;
-          break;
-      }
     } else {
       stream_->incByteIdx();
       b_ = B1;
@@ -121,10 +105,6 @@ void CJBig2_ArithDecoder::BYTEIN() {
     b_ = stream_->getCurByte_arith();
     c_ = c_ + 0xff00 - (b_ << 8);
     ct_ = 8;
-  }
-
-  if (!stream_->IsInBounds()) {
-    complete_ = true;
   }
 }
 
