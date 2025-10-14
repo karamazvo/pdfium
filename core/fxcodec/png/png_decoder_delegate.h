7bbad7845512425e285fe5d78f87303103ef4079
Lukasz Anforowicz <lukasza@google.com>
2025-10-14 21:01:36 +0000

    [rust png] Support using Rust-based PNG codec in XFA implementation.
    After this CL, if `pdf_use_skia && pdf_enable_rust_png` then the
    Rust-based PNG codec will be used in XFA implementation.
    
    Note that the CL at https://pdfium-review.googlesource.com/136790 shows
    that PDFium CQ covers the new code (i.e. an artificial problem
    introduced by this CL results in failures in `mac_xfa_skia_rust` and
    `win_xfa_skia_rust`).
    
    Bug: 444045690
    Cq-Include-Trybots: luci.pdfium.try:mac_xfa_skia_rust
    Cq-Include-Trybots: luci.pdfium.try:win_xfa_skia_rust
    Change-Id: Ia6ec12d0335b1fa74c278acc94973e97aaa30b60

--- core/fxcodec/png/png_decoder_delegate.h
+++ core/fxcodec/png/png_decoder_delegate.h
@@ -9,12 +9,8 @@
 
 namespace fxcodec {
 
-// Abstract interface used by the `libpng`-based decoder from `png_decoder.h`
-// (in the future by Skia-based decoder from `skia_png_decoder.h`).
-//
-// TODO(https://crbug.com/444045690): Update the comment above once the
-// Skia-based decoder is implemented and/or the `libpng`-based decoder moved
-// to renamed .h/.cc files.
+// Abstract interface used by the `libpng`-based decoder from
+// `libpng_png_decoder.h` and from `skia_png_decoder.cpp`.
 class PngDecoderDelegate {
  public:
   // Color type specified in the PNG IHDR chunk.  See also
