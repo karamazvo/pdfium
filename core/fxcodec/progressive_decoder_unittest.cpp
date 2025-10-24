8ebd2d9f25205e6e622c8267940144e9b8d15b72
Lukasz Anforowicz <lukasza@google.com>
2025-10-24 21:12:44 +0000

    [rust png] Add explicit `PngFinishedDecoding` callback.
    Before this CL, `ProgressiveDecoder` would keep calling into
    `PngDecoder::ContinueDecode` until consuming the whole `file_`.
    After this CL, we rely on a new `PngFinishedDecoding` callback instead.
    
    This approach is:
    
    * Consistent with how other codecs work.  For example see how the loops
      in `ProgressiveDecoder::BmpContinueDecode` and/or
      `ProgressiveDecoder::GifContinueDecode` continue until the codec
      indicates that the full image has been decoded.
    * More robust - for example a truncated input should not anymore be
      treated as a success (before this CL the new `TruncatedPng` unit tests
      would have ended with `kDecodeFinished` rather than `kError`).
    
    Bug: 444045690
    Change-Id: Iffbe0d2e36471af9ab5a0157fd19abefa7f01c75

--- core/fxcodec/progressive_decoder_unittest.cpp
+++ core/fxcodec/progressive_decoder_unittest.cpp
@@ -562,6 +562,43 @@ TEST_F(ProgressiveDecoderTest, PngSmokeTest) {
   EXPECT_THAT(bitmap->GetScanline(0).first(4u),
               ElementsAre(0x00, 0xFF, 0x00, 0xFF));
 }
+
+TEST_F(ProgressiveDecoderTest, TruncatedPng) {
+  // `kInput` has been taken from Chromium's
+  // `//third_party/blink/renderer/platform/testing/data/green.png`
+  // but truncated after ~8 bytes of IDAT chunk's payload.
+  static constexpr uint8_t kInput[] = {
+      0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00,
+      0x0d, 0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x64, 0x00, 0x00,
+      0x00, 0x32, 0x01, 0x03, 0x00, 0x00, 0x00, 0x90, 0xfb, 0xec, 0xfd,
+      0x00, 0x00, 0x00, 0x03, 0x50, 0x4c, 0x54, 0x45, 0x00, 0xff, 0x00,
+      0x34, 0x5e, 0xc0, 0xa8, 0x00, 0x00, 0x00, 0x0f, 0x49, 0x44, 0x41,
+      0x54, 0x28, 0x15, 0x63, 0x60, 0x18, 0x05, 0xa3, 0x60};
+
+  ProgressiveDecoder decoder;
+
+  auto source = pdfium::MakeRetain<CFX_ReadOnlySpanStream>(kInput);
+  CFX_DIBAttribute attr;
+  FXCODEC_STATUS status =
+      decoder.LoadImageInfo(std::move(source), FXCODEC_IMAGE_PNG, &attr, true);
+  ASSERT_EQ(FXCODEC_STATUS::kFrameReady, status);
+
+  ASSERT_EQ(100, decoder.GetWidth());
+  ASSERT_EQ(50, decoder.GetHeight());
+  ASSERT_EQ(FXDIB_Format::kBgra, decoder.GetBitmapFormat());
+
+  auto bitmap = pdfium::MakeRetain<CFX_DIBitmap>();
+  ASSERT_TRUE(bitmap->Create(decoder.GetWidth(), decoder.GetHeight(),
+                             decoder.GetBitmapFormat()));
+
+  size_t frames;
+  std::tie(status, frames) = decoder.GetFrames();
+  ASSERT_EQ(FXCODEC_STATUS::kDecodeReady, status);
+  ASSERT_EQ(1u, frames);
+
+  status = DecodeToBitmap(decoder, bitmap);
+  EXPECT_EQ(FXCODEC_STATUS::kError, status);
+}
 #endif
 
 }  // namespace fxcodec
