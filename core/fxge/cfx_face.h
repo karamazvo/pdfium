29b26d8417e6994107673820d557ede9c2f94f25
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-10-30 10:45:34 +0530

    Consolidate determination code for font styles
    core/fxge/android/cfpf_skiafontmgr.cpp and
    xfa/fgas/font/cfgas_fontmgr.cpp utilize nearly common code for font
    style determination. This CL consolidates this logic into one file to
    reduce duplicate code.
    
    The function (in fx_font.cpp), returns a struct FontStyleInfo with
    uint32_t values for font style and os2CodepageMask which is equal to
    code_page_range.value()[0] for finding the charset in some callers.
    
    Bug: 452079357
    Change-Id: I3de610c9201e68312fce72367eb142b7c37aad22

--- core/fxge/cfx_face.h
+++ core/fxge/cfx_face.h
@@ -93,7 +93,6 @@ class CFX_Face final : public Retainable, public Observable {
 
   std::optional<std::array<uint32_t, 4>> GetOs2UnicodeRange();
   std::optional<std::array<uint32_t, 2>> GetOs2CodePageRange();
-  std::optional<std::array<uint8_t, 2>> GetOs2Panose();
 
   int GetGlyphCount() const;
   // TODO(crbug.com/pdfium/2037): Can this method be private?
@@ -142,6 +141,13 @@ class CFX_Face final : public Retainable, public Observable {
   FXFT_FaceRec* GetRec() { return rec_.get(); }
   const FXFT_FaceRec* GetRec() const { return rec_.get(); }
 
+  struct FontStyleInfo {
+    uint32_t style;
+    uint32_t os2_codepage_mask;
+  };
+
+  FontStyleInfo GetFontStyleInfo(const RetainPtr<CFX_Face>& face);
+
  private:
   CFX_Face(FXFT_FaceRec* pRec, RetainPtr<Retainable> pDesc);
   ~CFX_Face() override;
@@ -150,6 +156,7 @@ class CFX_Face final : public Retainable, public Observable {
 
   ScopedFXFTFaceRec const rec_;
   RetainPtr<Retainable> const desc_;
+  std::optional<std::array<uint8_t, 2>> GetOs2Panose();
 };
 
 #endif  // CORE_FXGE_CFX_FACE_H_
