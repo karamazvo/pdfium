30b959a659d1c5c58e98ef41f9030c9eb3eca834
April Kallmeyer <ask@chromium.org>
2025-11-03 18:43:53 +0000

    [Skia] Optimize DrawPath() when knockout is on
    Instead of calculating geometric union and subtraction of paths which
    may contain many points, composite the objects in pixel-space using
    blend modes.
    
    According to https://crbug.com/334774307#comment21 this implementation
    should be non-lossy since the saveLayer system should allow the mask
    resolution to scale and operations replayed.
    
    Initially I kept the same structure of doing a union or difference based
    on the stroke color and drawing the stroke on the separate layer from
    the fill. However when I read ISO 32000-1:2008 I realized I could
    simplify it and make the function more accurately follow the spec.
    
    Rendering performance for the pdf in the bug using pdfium_test:
    
     - Before: 68.72s user 0.37s system 99% cpu 1:09.14 total
     - After: 0.52s user 0.12s system 99% cpu 0.645 total
    
    Bug: 334774307
    Change-Id: I7cc2d264bb06182c69d74dc7fcd04fe285d47256

--- fpdfsdk/fpdf_edit_embeddertest.cpp
+++ fpdfsdk/fpdf_edit_embeddertest.cpp
@@ -2697,7 +2697,7 @@ TEST_F(FPDFEditEmbedderTest, AddStrokedPaths) {
     ScopedFPDFBitmap page_bitmap = RenderPage(page);
     const char* checksum_3 = []() {
       if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
-        return "a5de6ddefcbae60924bebc99347e460b";
+        return "037f5b38d8b612abf0833eb9ac8adf69";
       }
       return "ff3e6a22326754944cc6e56609acd73b";
     }();
@@ -3943,7 +3943,7 @@ TEST_F(FPDFEditEmbedderTest, LoadCidType2FontWithBadParameters) {
 TEST_F(FPDFEditEmbedderTest, SaveAndRender) {
   const char* checksum = []() {
     if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
-      return "edd4aed776c0eaf8c79dd24d9654af95";
+      return "a6e8827e9fda09151765130e2f5531eb";
     }
     return "3c20472b0552c0c22b88ab1ed8c6202b";
   }();
