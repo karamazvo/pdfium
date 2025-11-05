651170a81f341be5450891a1900fbcf3ef0988d8
Lei Zhang <thestig@chromium.org>
2025-11-05 10:39:20 -0800

    Rename "pContext" instances to "context" or "ctx"
    Mass rename a common variable name to follow Google C++ style via search
    and replace. Lean on the compiler to find shadow variables and use "ctx"
    as the alternate name for those cases.
    
    Bug: 42271580
    Change-Id: Ic09613ee36927023127b44f2a1fd671a59b05295

--- core/fxcodec/bmp/bmp_decoder.cpp
+++ core/fxcodec/bmp/bmp_decoder.cpp
@@ -19,11 +19,11 @@ namespace fxcodec {
 // static
 std::unique_ptr<ProgressiveDecoderContext> BmpDecoder::StartDecode(
     Delegate* pDelegate) {
-  return std::make_unique<CFX_BmpContext>(pDelegate);
+  return std::make_unique<CFX_Bmcontext>(pDelegate);
 }
 
 // static
-BmpDecoder::Status BmpDecoder::ReadHeader(ProgressiveDecoderContext* pContext,
+BmpDecoder::Status BmpDecoder::ReadHeader(ProgressiveDecoderContext* context,
                                           int32_t* width,
                                           int32_t* height,
                                           bool* tb_flag,
@@ -32,7 +32,7 @@ BmpDecoder::Status BmpDecoder::ReadHeader(ProgressiveDecoderContext* pContext,
                                           CFX_DIBAttribute* pAttribute) {
   DCHECK(pAttribute);
 
-  auto* ctx = static_cast<CFX_BmpContext*>(pContext);
+  auto* ctx = static_cast<CFX_Bmcontext*>(context);
   Status status = ctx->bmp_.ReadHeader();
   if (status != Status::kSuccess) {
     return status;
@@ -50,19 +50,19 @@ BmpDecoder::Status BmpDecoder::ReadHeader(ProgressiveDecoderContext* pContext,
 }
 
 // static
-BmpDecoder::Status BmpDecoder::LoadImage(ProgressiveDecoderContext* pContext) {
-  return static_cast<CFX_BmpContext*>(pContext)->bmp_.DecodeImage();
+BmpDecoder::Status BmpDecoder::LoadImage(ProgressiveDecoderContext* context) {
+  return static_cast<CFX_Bmcontext*>(context)->bmp_.DecodeImage();
 }
 
 // static
-FX_FILESIZE BmpDecoder::GetAvailInput(ProgressiveDecoderContext* pContext) {
-  return static_cast<CFX_BmpContext*>(pContext)->bmp_.GetAvailInput();
+FX_FILESIZE BmpDecoder::GetAvailInput(ProgressiveDecoderContext* context) {
+  return static_cast<CFX_Bmcontext*>(context)->bmp_.GetAvailInput();
 }
 
 // static
-bool BmpDecoder::Input(ProgressiveDecoderContext* pContext,
+bool BmpDecoder::Input(ProgressiveDecoderContext* context,
                        RetainPtr<CFX_CodecMemory> codec_memory) {
-  auto* ctx = static_cast<CFX_BmpContext*>(pContext);
+  auto* ctx = static_cast<CFX_Bmcontext*>(context);
   ctx->bmp_.SetInputBuffer(std::move(codec_memory));
   return true;
 }
