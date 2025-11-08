52bf8f1425dbff14acdd65adc121f5e479869e98
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-11-08 08:45:41 +0530

    Consolidate CFX_Face::Open() callers
    This changeset consolidates the code in callers of CFX_Face::Open(). The
    functions CFX_Font::LoadFile() and LoadFace() in cfgas_fontmgr.cpp are
    two callers which share nearly identical logic. Though with fewer shared
    aspects, android/cfpf_skiafontmgr.cpp also uses the consolidated code.
    
    The code is consolidated into a function called OpenFromStream() and an
    edited Open() function, with the former invoking the latter. These
    functions are utilised in the callers to continue working toward the
    goals of freetype encapsulation.
    
    Bug: 452087741
    Change-Id: Ic745d6b3c29b0736c38aaddafcc76e815976ac96

--- core/fxge/cfx_font.h
+++ core/fxge/cfx_font.h
@@ -142,8 +142,6 @@ class CFX_Font {
   ByteString GetFamilyNameOrUntitled() const;
 
 #if defined(PDF_ENABLE_XFA)
-  // |owned_file_| must outlive |owned_stream_rec_|.
-  RetainPtr<IFX_SeekableReadStream> owned_file_;
   std::unique_ptr<FXFT_StreamRec> owned_stream_rec_;  // Must outlive |face_|.
 #endif
 
