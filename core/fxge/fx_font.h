c3380e6ad66e9aa4de9374c5efac9da8ac4f4df4
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-11-22 13:20:39 +0530

    Decouple anti-alias setting from FT_Render_Mode
    This CL decouples the anti-alias setting from the previous freetype
    callers. This is done by introducing a new enum, AARenderMode, and a
    function GetFTRenderMode(), which is used to abstract the antialiasing
    setting.
    
    This change is part of the ongoing effort to encapsulate FreeType usage,
    which makes future adjustments to anti-aliasing logic easier and
    cleaner.
    
    Change-Id: I6ef3d17ca6af94972e48691311df088f5cb6f64d
    Bug: 452087737

--- core/fxge/fx_font.h
+++ core/fxge/fx_font.h
@@ -46,8 +46,19 @@ enum FontWeight {
   kFontWeightBold = 700,
   kFontWeightExtraBold = 900,
 };
-
 }  // namespace pdfium
+
+enum class FontAntiAliasingMode : int {
+  kNORMAL,
+  kLIGHT,
+  kMONO,
+  kLCD,
+  kLCD_V,
+  kSDF,
+  kMAX
+};
+
+int GetFTRenderMode(FontAntiAliasingMode mode);
 
 /* Other font flags */
 #define FXFONT_USEEXTERNATTR 0x80000
@@ -65,7 +76,8 @@ using CFX_TypeFace = SkTypeface;
 
 class TextGlyphPos;
 
-FX_RECT GetGlyphsBBox(const std::vector<TextGlyphPos>& glyphs, int anti_alias);
+FX_RECT GetGlyphsBBox(const std::vector<TextGlyphPos>& glyphs,
+                      FontAntiAliasingMode anti_alias);
 
 ByteString GetNameFromTT(pdfium::span<const uint8_t> name_table, uint32_t name);
 size_t GetTTCIndex(pdfium::span<const uint8_t> font_data, size_t font_offset);
