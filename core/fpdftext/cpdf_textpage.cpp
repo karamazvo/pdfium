dad30c7f113d83ed4fd0bcba475265b7de0dc7e8
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-12-07 12:31:58 +0530

    Remove character width check to fix missing text
    Initially, logic in CPDF_TextPage::ProcessTextObject was discarding
    spaces (and characters with small widths) by checking
    fabs(pTextObj>GetRect().Width()) < kSizeEpsilon.
    
    This patch removes said checks and updates test expectations to fit the
    new behaviour. This patch also adds new tests and updates old ones
    NeedsImprovement results for tests that have been fixed by the removal
    of this aforementioned check.
    
    Bug: 444176962, 40643656
    Change-Id: I99a6a5f0c4ccd1825437482845b597272f297d2f

--- core/fpdftext/cpdf_textpage.cpp
+++ core/fpdftext/cpdf_textpage.cpp
@@ -854,6 +854,12 @@ void CPDF_TextPage::CloseTempLine() {
     }
     bPrevSpace = true;
   }
+  if (str.GetLength() >= 2 && str[str.GetLength() - 1] == ' ' &&
+      str[str.GetLength() - 2] != ' ') {
+    temp_text_buf_.Delete(str.GetLength() - 1, 1);
+    temp_char_list_.erase(temp_char_list_.begin() + str.GetLength() - 1);
+    str.Delete(str.GetLength() - 1);
+  }
   CFX_BidiString bidi(str);
   if (rtl_) {
     bidi.SetOverallDirectionRight();
@@ -885,10 +891,6 @@ void CPDF_TextPage::ProcessTextObject(
     const CFX_Matrix& form_matrix,
     const CPDF_PageObjectHolder* pObjList,
     CPDF_PageObjectHolder::const_iterator ObjPos) {
-  if (fabs(pTextObj->GetRect().Width()) < kSizeEpsilon) {
-    return;
-  }
-
   size_t count = text_objects_.size();
   TransformedTextObject new_obj;
   new_obj.text_obj_ = pTextObj;
@@ -1083,9 +1085,6 @@ void CPDF_TextPage::SwapTempTextBuf(size_t iCharListStartAppend,
 
 void CPDF_TextPage::ProcessTextObject(const TransformedTextObject& obj) {
   CPDF_TextObject* const pTextObj = obj.text_obj_;
-  if (fabs(pTextObj->GetRect().Width()) < kSizeEpsilon) {
-    return;
-  }
 
   const CFX_Matrix form_matrix = obj.form_matrix_;
   const MarkedContentState ePreMKC = PreMarkedContent(pTextObj);
