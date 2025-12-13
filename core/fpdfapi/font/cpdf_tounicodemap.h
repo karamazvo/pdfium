ad23c56da43065fe0bd9b1ee373fa80eadd4ba36
Lei Zhang <thestig@chromium.org>
2025-12-12 16:13:42 -0800

    Partially fix CPDFToUnicodeMapTest.NonBmpUnicodeLookup
    Make the lookup work on many POSIX platforms where wchar_t is 32-bit.
    
    Start by getting rid of GetMultiCharIndexIndicator() and
    `multi_char_vec_`. The synthesized values GetMultiCharIndexIndicator()
    generates are problematic because they may overlap with values in
    charmaps. Instead, change `map_` to store the lookup result directly,
    instead of potentially doing a secondary lookup in `multi_char_vec_`.
    Update InsertIntoMaps() to keep in sync with the data changes, and add
    InsertIntoMapsMultiChar() to handle the multi-char code.
    
    For the reverse mapping in InsertIntoMapsMultiChar(), use
    SurrogatePair to decode the WideString and get the code point. Use the
    code point as the key in `reverse_map_`.
    
    As a result of the changes here, CPDF_ToUnicodeMap::Lookup(0xffff) now
    works as expected in the HandleBeginBFRangeDestLargeValue test case.
    
    Bug: 374947848
    Change-Id: Ia8693184e0eb8408dd60aaebb4141181aff1c5cb

--- core/fpdfapi/font/cpdf_tounicodemap.h
+++ core/fpdfapi/font/cpdf_tounicodemap.h
@@ -9,7 +9,7 @@
 
 #include <map>
 #include <optional>
-#include <vector>
+#include <variant>
 
 #include "core/fxcrt/fx_string.h"
 #include "core/fxcrt/retain_ptr.h"
@@ -45,18 +45,18 @@ class CPDF_ToUnicodeMap {
   ByteStringView HandleBeginBFRange(CPDF_SimpleParser& parser,
                                     ByteStringView previous_word);
 
-  uint32_t GetMultiCharIndexIndicator() const;
   void SetCode(uint32_t srccode, WideString destcode);
 
   // Inserts a new entry into `map_` and `reverse_map_`.
-  void InsertIntoMaps(uint32_t code, uint32_t destcode);
+  void InsertIntoMaps(uint32_t code,
+                      std::variant<uint32_t, WideString> destcode);
 
   // Key: charcode
   // Value: unicode
   // If there are multiple entries with the same key, this stores the lowest
   // value.
-  std::map<uint32_t, uint32_t> map_;
-  // Key: unicode
+  std::map<uint32_t, WideString> map_;
+  // Key: unicode code point
   // Value: charcode
   // Since `map_` may encounter entries with the same key but different values,
   // that situation does not cause a conflict in `reverse_map_`. Thus
@@ -66,7 +66,6 @@ class CPDF_ToUnicodeMap {
   std::map<uint32_t, uint32_t> reverse_map_;
 
   UnownedPtr<const CPDF_CID2UnicodeMap> base_map_;
-  std::vector<WideString> multi_char_vec_;
 };
 
 #endif  // CORE_FPDFAPI_FONT_CPDF_TOUNICODEMAP_H_
