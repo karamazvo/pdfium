d4b7270158db2c645efc7d85fa4f274301d45683
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-12-16 09:17:30 +0530

    Use PNG-based comparison for 612x792 blank page
    This CL adds the image blank_612x792 and utilizes
    EmbedderTest::CompareBitmapToPng*() to replace the old checksum-based
    system previously used to verify rendering in embeddertests.
    
    The CL also creates the function TestRenderPageSkpToPng to test skia
    rendering in comparison to PNGs rather than the prior md5 checksum. This
    move allows for a visual verification that tests are functioning making
    them more robust than an md5 checksum.
    
    Bug: 468228360
    Change-Id: I79d17457c57668d12d9eaa050abbda1082c75375

--- fpdfsdk/fpdf_edit_embeddertest.cpp
+++ fpdfsdk/fpdf_edit_embeddertest.cpp
@@ -2461,10 +2461,10 @@ TEST_F(FPDFEditEmbedderTest, AddAndRemovePaths) {
   ASSERT_TRUE(page);
 
   // Render the blank page and verify it's a blank bitmap.
-  using pdfium::kBlankPage612By792Checksum;
+  using pdfium::kBlankPage612By792Png;
   {
     ScopedFPDFBitmap page_bitmap = RenderPage(page);
-    CompareBitmap(page_bitmap.get(), 612, 792, kBlankPage612By792Checksum);
+    CompareBitmapToPng(page_bitmap.get(), pdfium::kBlankPage612By792Png);
   }
   ASSERT_EQ(0, FPDFPage_CountObjects(page));
 
@@ -2485,7 +2485,7 @@ TEST_F(FPDFEditEmbedderTest, AddAndRemovePaths) {
   EXPECT_TRUE(FPDFPage_RemoveObject(page, red_rect));
   {
     ScopedFPDFBitmap page_bitmap = RenderPage(page);
-    CompareBitmap(page_bitmap.get(), 612, 792, kBlankPage612By792Checksum);
+    CompareBitmapToPng(page_bitmap.get(), pdfium::kBlankPage612By792Png);
   }
   EXPECT_EQ(0, FPDFPage_CountObjects(page));
 
@@ -2837,11 +2837,10 @@ TEST_F(FPDFEditEmbedderTest, AddStandardFontTextOfSizeZero) {
   EXPECT_TRUE(FPDFPage_GenerateContent(page.get()));
   {
     ScopedFPDFBitmap page_bitmap = RenderPage(page.get());
-    CompareBitmap(page_bitmap.get(), 612, 792,
-                  pdfium::kBlankPage612By792Checksum);
+    CompareBitmapToPng(page_bitmap.get(), pdfium::kBlankPage612By792Png);
 
     EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
-    VerifySavedDocument(612, 792, pdfium::kBlankPage612By792Checksum);
+    VerifySavedDocumentToPng(pdfium::kBlankPage612By792Png);
   }
 }
 
