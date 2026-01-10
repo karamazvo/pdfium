e540bc382c49fe25ef9ed915133db83c54befa52
Lei Zhang <thestig@chromium.org>
2026-01-09 21:54:43 -0800

    Store raw_spans in CJBig2_GRDProc
    Change `CJBig2_GRDProc::line_` from a pointer to a raw_span. Since spans
    cannot go in the negative direction, also store 2 more spans for the
    previous lines. Then add CJBig2_GRDProc::FinishedDecode() so callers can
    clear these spans and avoid dangling pointers.
    
    Change-Id: Id9df2bebf0206ecd8d7b2e262d25520d6f3a47a4

--- core/fxcodec/jbig2/JBig2_PddProc.cpp
+++ core/fxcodec/jbig2/JBig2_PddProc.cpp
@@ -47,6 +47,7 @@ std::unique_ptr<CJBig2_PatternDict> CJBig2_PDDProc::DecodeArith(
   while (status == FXCODEC_STATUS::kDecodeToBeContinued) {
     status = pGRD->ContinueDecode(&state);
   }
+  pGRD->FinishedDecode();
   if (!BHDC) {
     return nullptr;
   }
