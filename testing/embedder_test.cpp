5bd7915adbcc97a91758c623049488a51c1092f4
LearnPRG-py <aryankrishnan4b@gmail.com>
2026-01-15 23:05:21 +0530

    Replace Checksums with Images in fpdf_view_embeddertest.cpp
    This CL aims to remove md5 checksums and replace them with expectation
    images in fpdf_view_embeddertest.cpp. This CL also adds functions to
    verify greyscale images which are required to verify rendering of
    bitmaps with flags such as FPDF_GRAYSCALE. In addition to this, the Cl
    also removes the old, non FooToPng() methods to eventually make PNG
    comparison the default. The use of images provides a clearer
    visualisation of rendering and acts as a more robust means of
    verification than the previously used MD5 checksums.
    
    Bug: 468228360
    Change-Id: I4dafc487266b84842ae6b698e7b8a469ba8caad6

--- testing/embedder_test.cpp
+++ testing/embedder_test.cpp
@@ -19,6 +19,7 @@
 #include "core/fxcrt/notreached.h"
 #include "core/fxcrt/numerics/checked_math.h"
 #include "core/fxcrt/numerics/safe_conversions.h"
+#include "core/fxcrt/span_util.h"
 #include "core/fxge/cfx_defaultrenderdevice.h"
 #include "core/fxge/dib/fx_dib.h"
 #include "fpdfsdk/cpdfsdk_helpers.h"
@@ -373,6 +374,31 @@ int CompareBGRxBitmapToPng(pdfium::span<const uint8_t> bitmap_span,
   return pixels_different;
 }
 
+int CompareGrayBitmapToPng(pdfium::span<const uint8_t> bitmap_span,
+                           size_t bitmap_stride,
+                           const DecodedPng& decoded_png,
+                           int max_pixel_per_channel_delta) {
+  const int width = decoded_png.width;
+  const size_t dest_row_width = static_cast<size_t>(width);
+  const int height = decoded_png.height;
+  const size_t bgrx_stride = width * sizeof(FX_BGRA_STRUCT<uint8_t>);
+  std::vector<uint8_t> bgrx_buffer(bgrx_stride * height);
+  auto bgrx_span = fxcrt::reinterpret_span<FX_BGRA_STRUCT<uint8_t>>(
+      pdfium::span<uint8_t>(bgrx_buffer));
+  for (int h = 0; h < height; ++h) {
+    auto src_row = bitmap_span.subspan(h * bitmap_stride, bitmap_stride);
+    auto dest_row = bgrx_span.take_first(dest_row_width);
+    for (auto [src_pixel, dest_pixel] : fxcrt::zip(src_row, dest_row)) {
+      dest_pixel.blue = src_pixel;
+      dest_pixel.green = src_pixel;
+      dest_pixel.red = src_pixel;
+      dest_pixel.alpha = 255;
+    }
+  }
+  return CompareBGRxBitmapToPng(bgrx_buffer, bgrx_stride, decoded_png,
+                                max_pixel_per_channel_delta);
+}
+
 #ifdef PDF_USE_SKIA
 int CompareBGRxPremultBitmapToPng(pdfium::span<const uint8_t> bitmap_span,
                                   size_t bitmap_stride,
@@ -458,6 +484,10 @@ void CompareBitmapToPngData(FPDF_BITMAP bitmap,
           bitmap_span, stride, decoded_png, max_pixel_per_channel_delta);
       break;
     }
+    case FPDFBitmap_Gray:
+      pixels_different = CompareGrayBitmapToPng(
+          bitmap_span, stride, decoded_png, max_pixel_per_channel_delta);
+      break;
 #ifdef PDF_USE_SKIA
     case FPDFBitmap_BGRA_Premul:
       pixels_different =
