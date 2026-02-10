36d64fee1736cf4bc4e7f4fa72389b4bb070b7cf
Andy Phan <andyphan@chromium.org>
2026-02-10 10:36:13 -0800

    Add FPDF_SUBSET_NEW_FONTS flag to FPDF_SaveAsCopy()
    Add a new FPDF_SUBSET_NEW_FONTS flag, which will be used to subset new
    fonts embedded into the saved PDF. Then, add tests and TODOs.
    
    For now, this flag is a no-op. Font subsetting will be implemented in
    https://pdfium-review.googlesource.com/c/pdfium/+/141854.
    
    Bug: 476127152
    Change-Id: I25fa7e8acebae5ac917928c4422c0cde5b7bc0d1

--- public/fpdf_save.h
+++ public/fpdf_save.h
@@ -49,6 +49,8 @@ typedef struct FPDF_FILEWRITE_ {
  // TODO(crbug.com/42270430): Remove FPDF_REMOVE_SECURITY_DEPRECATED.
 #define FPDF_REMOVE_SECURITY_DEPRECATED 3
 #define FPDF_REMOVE_SECURITY (1 << 2)
+// Experimental.
+#define FPDF_SUBSET_NEW_FONTS (1 << 3)
 
 // Function: FPDF_SaveAsCopy
 //          Saves the copy of specified document in custom way.
