f6db06a802b9294c29e33b9219f083257259b178
Andy Phan <andyphan@chromium.org>
2026-02-11 11:57:20 -0800

    Update CPDF_Creator::Create() flags to match FPDF_SaveAsCopy() flags
    CPDF_Creator::Create() flags should be kept in sync with
    FPDF_SaveAsCopy() flags. Update the existing values to use bit shifting
    and add missing flags. Then, add static_asserts to ensure these values
    are in sync.
    
    https://pdfium-review.googlesource.com/c/pdfium/+/142131 will move flag
    parsing from FPDF_SaveAsCopy() to CPDF_Creator::Create() and use the new
    flags.
    
    Also remove an unnecessary bit flip in that was added in
    https://pdfium-review.googlesource.com/c/pdfium/+/142591. The bit is
    ignored in CPDF_Creator.
    
    Bug: 42270430, 476127152
    Change-Id: I30f0728edd6280d0efaf788ee6e9b322bc4a6fe8

--- fpdfsdk/fpdf_save.cpp
+++ fpdfsdk/fpdf_save.cpp
@@ -31,6 +31,13 @@
 #include "public/fpdf_formfill.h"
 #endif
 
+static_assert(FPDF_INCREMENTAL == CPDF_Creator::SaveFlag::kIncremental);
+static_assert(FPDF_NO_INCREMENTAL == CPDF_Creator::SaveFlag::kNoOriginal);
+static_assert(FPDF_REMOVE_SECURITY_DEPRECATED ==
+              CPDF_Creator::SaveFlag::kRemoveSecurityDeprecated);
+static_assert(FPDF_REMOVE_SECURITY == CPDF_Creator::SaveFlag::kRemoveSecurity);
+static_assert(FPDF_SUBSET_NEW_FONTS == CPDF_Creator::SaveFlag::kSubsetNewFonts);
+
 namespace {
 
 constexpr int kAllValidFlags = FPDF_INCREMENTAL | FPDF_NO_INCREMENTAL |
@@ -192,7 +199,6 @@ bool DoDocSave(FPDF_DOCUMENT document,
 
   if (flags == FPDF_REMOVE_SECURITY_DEPRECATED ||
       (flags & FPDF_REMOVE_SECURITY)) {
-    flags &= ~FPDF_REMOVE_SECURITY;
     file_maker.RemoveSecurity();
   }
 
@@ -201,7 +207,8 @@ bool DoDocSave(FPDF_DOCUMENT document,
   }
 
   // TODO(crbug.com/476127152): Subset new fonts.
-  bool create_result = file_maker.Create(static_cast<uint32_t>(flags));
+  bool create_result =
+      file_maker.Create(static_cast<CPDF_Creator::SaveFlag>(flags));
 
 #ifdef PDF_ENABLE_XFA
   if (context) {
