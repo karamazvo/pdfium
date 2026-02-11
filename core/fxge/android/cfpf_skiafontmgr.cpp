26b1967f830de9a515f8eb396f123ca97d28d268
Tom Sepez <tsepez@google.com>
2026-02-11 23:52:32 +0000

    [android] Remove file-path FT_Open_Face calls.
    Use the shared-memory backed streams. Note that the CFX_Face
    keeps the stream alive which keeps the mapping alive. Also note
    that we believe that FT would keep an open FD for each call to
    FT_OPEN_PATHNAME, so that should not change. However, there
    will be more memory mapped segments for the OS to deal with.
    
    This removes the file-based loading, which isn't supported by
    Fontations down the road.
    
    -- Remove platform-specific ifdefs.  Should build on all, maybe
       experiment with other platforms sometime.
    
    Change-Id: Ie38501d5cb046df0b9cfd703105dec4cb60557b8

--- core/fxge/android/cfpf_skiafontmgr.cpp
+++ core/fxge/android/cfpf_skiafontmgr.cpp
@@ -12,6 +12,7 @@
 #include <iterator>
 #include <utility>
 
+#include "core/fxcrt/cfx_read_only_container_stream.h"
 #include "core/fxcrt/compiler_specific.h"
 #include "core/fxcrt/containers/adapters.h"
 #include "core/fxcrt/fx_codepage.h"
@@ -329,9 +330,11 @@ CFPF_SkiaFont* CFPF_SkiaFontMgr::CreateFont(ByteStringView family_name,
   return ret;
 }
 
-RetainPtr<CFX_Face> CFPF_SkiaFontMgr::GetFontFace(ByteStringView path,
+RetainPtr<CFX_Face> CFPF_SkiaFontMgr::GetFontFace(const ByteString& path,
                                                   int32_t face_index) {
-  return CFX_Face::OpenFromFilePath(font_mgr_, path, face_index);
+  return CFX_Face::NewFromSpanStream(
+      font_mgr_, pdfium::MakeRetain<CFX_ReadOnlyByteStringStream>(path),
+      face_index);
 }
 
 void CFPF_SkiaFontMgr::ScanPath(const ByteString& path) {
@@ -367,7 +370,7 @@ void CFPF_SkiaFontMgr::ScanPath(const ByteString& path) {
 
 void CFPF_SkiaFontMgr::ScanFile(const ByteString& file) {
   constexpr int kFaceIndex = 0;
-  RetainPtr<CFX_Face> face = GetFontFace(file.AsStringView(), kFaceIndex);
+  RetainPtr<CFX_Face> face = GetFontFace(file, kFaceIndex);
   if (!face) {
     return;
   }
