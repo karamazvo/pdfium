5c40b8de2a2fc7b4196a184a0b3691fcd83441c5
Andy Phan <andyphan@chromium.org>
2026-02-13 13:06:05 -0800

    Introduce CPDF_FontSubsetter to subset new fonts on PDF save
    Add CPDF_FontSubsetter, which, when given a list of new PDF object
    numbers, will output object overrides required to subset any new fonts
    while saving the PDF. The objects do not affect the original copy and
    are purely used for writing the new PDF, avoiding any in-memory PDF
    mutation. CPDF_FontSubsetter uses HarfBuzz internally to subset fonts.
    
    There are still some remaining TODOs to make the subsetted font
    compliant with the PDF spec, but some PDF viewers will be able to
    properly render the subsetted font with this CL alone.
    
    Bug: 476127152
    Change-Id: I0c16be9a15c295be231aa34735f5d2e4edbc9d1b

---
+++ core/fpdfapi/edit/cpdf_fontsubsetter.h
@@ -0,0 +1,75 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FPDFAPI_EDIT_CPDF_FONTSUBSETTER_H_
+#define CORE_FPDFAPI_EDIT_CPDF_FONTSUBSETTER_H_
+
+#include <stdint.h>
+
+#include <map>
+#include <set>
+
+#include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fxcrt/retain_ptr.h"
+#include "core/fxcrt/span.h"
+#include "core/fxcrt/unowned_ptr.h"
+
+class CPDF_Font;
+class CPDF_Object;
+class CPDF_Page;
+class CPDF_Stream;
+class CPDF_TextObject;
+
+// CPDF_FontSubsetter generate object overrides for any text-related PDF objects
+// in order to subset new embedded fonts. CPDF_FontSubsetter only creates new
+// PDF objects and does not modify any existing PDF objects. It should be used
+// during saving to create objects that should replace existing objects when
+// writing the PDF.
+class CPDF_FontSubsetter {
+ public:
+  explicit CPDF_FontSubsetter(CPDF_Document* doc);
+  ~CPDF_FontSubsetter();
+
+  // Given `new_obj_nums`, the collection of all new object numbers, returns a
+  // map of object numbers to new PDF objects that should override the original
+  // objects in order to subset any embedded fonts for new text.
+  std::map<uint32_t, RetainPtr<const CPDF_Object>> GenerateObjectOverrides(
+      pdfium::span<uint32_t> new_obj_nums);
+
+ private:
+  // Potential fonts that can be subsetted.
+  struct SubsetCandidate {
+    SubsetCandidate();
+    ~SubsetCandidate();
+
+    // PDF font-related objects that need to be overridden during the save.
+    // TODO(crbug.com/476127152): Override the root font, CID font, and
+    // descriptor.
+    RetainPtr<const CPDF_Stream> font_stream;
+
+    // The set of GIDs used by text.
+    std::set<uint32_t> used_gids;
+  };
+
+  // Gets the subset candidates from all pages.
+  void CollectSubsetCandidates();
+
+  // Gets the subset candidates from `page`.
+  void CollectSubsetCandidatesFromPage(CPDF_Page* page);
+
+  // Adds the characters used in `text` with `font` to `candidate`.
+  void AddUsedText(const CPDF_TextObject* text,
+                   CPDF_Font* font,
+                   SubsetCandidate& candidate);
+
+  UnownedPtr<CPDF_Document> const doc_;
+
+  // New PDF object numbers that were not in the original PDF.
+  std::set<uint32_t> new_obj_nums_;
+
+  // Map of font file stream object numbers to subset candidates.
+  std::map<uint32_t, CPDF_FontSubsetter::SubsetCandidate> candidates_;
+};
+
+#endif  // CORE_FPDFAPI_EDIT_CPDF_FONTSUBSETTER_H_
