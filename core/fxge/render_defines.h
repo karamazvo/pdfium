a0baffe8e99a4b631956f2ad7e7f962794d4cc5a
LearnPRG-py <aryankrishnan4b@gmail.com>
2026-02-16 21:29:35 +0530

    Add enums DeviceCapsId and RenderCapsFlag
    Add enums DeviceCapsId and RenderCapsFlag
    
    This CL replaces #define statements in render_defines.h with int enums. These help to move away from c-style code, allow for stricter type checking and help in moving the codebase to a future where #defines have to be less used.
    
    Bug: 42270078
    Change-Id: Ic67da34892c943e8041d6ee7bb278201a8aeec24

--- core/fxge/render_defines.h
+++ core/fxge/render_defines.h
@@ -7,26 +7,32 @@
 #ifndef CORE_FXGE_RENDER_DEFINES_H_
 #define CORE_FXGE_RENDER_DEFINES_H_
 
-#define FXDC_PIXEL_WIDTH 1
-#define FXDC_PIXEL_HEIGHT 2
-#define FXDC_BITS_PIXEL 3
-#define FXDC_HORZ_SIZE 4
-#define FXDC_VERT_SIZE 5
-#define FXDC_RENDER_CAPS 6
+enum class DeviceCapsId : int {
+  kPixelWidth,
+  kPixelHeight,
+  kBitsPixel,
+  kHorzSize,
+  kVertSize,
+  kRenderCaps,
+};
 
-#define FXRC_GET_BITS 0x01
-#define FXRC_ALPHA_PATH 0x02
-#define FXRC_ALPHA_IMAGE 0x04
-#define FXRC_ALPHA_OUTPUT 0x08
-#define FXRC_BLEND_MODE 0x10
-#define FXRC_SOFT_CLIP 0x20
-#define FXRC_BYTEMASK_OUTPUT 0x40
-// Assuming these are Skia-only for now. If this assumption changes, update both
-// the #if logic here, as well as the callsites that check these capabilities.
-#if defined(PDF_USE_SKIA)
-#define FXRC_FILLSTROKE_PATH 0x80
-#define FXRC_SHADING 0x100
-#define FXRC_PREMULTIPLIED_ALPHA 0x200
-#endif
+enum class RenderCapsFlag : int {
+  kGetBits = 0x01,
+  kAlphaPath = 0x02,
+  kAlphaImage = 0x04,
+  kAlphaOutput = 0x08,
+  kBlendMode = 0x10,
+  kSoftClip = 0x20,
+  kByteMaskOutput = 0x40,
+#ifdef PDF_USE_SKIA
+  kFillStrokePath = 0x80,
+  kShading = 0x100,
+  kPremultipliedAlpha = 0x200,
+#endif  // PDF_USE_SKIA
+};
+
+inline RenderCapsFlag operator|(RenderCapsFlag a, RenderCapsFlag b) {
+  return static_cast<RenderCapsFlag>(static_cast<int>(a) | static_cast<int>(b));
+}
 
 #endif  // CORE_FXGE_RENDER_DEFINES_H_
