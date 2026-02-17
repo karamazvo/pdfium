6f0e260488021eabd6d9e0ab6f706b9bf257d5db
Tom Sepez <tsepez@google.com>
2026-02-17 19:22:21 +0000

    Fix PathService::GetThirdPartyFilePath() on Android.
    The current logic is dubious at best (looking for other random third
    party directory to determine build configuration) and unworkable on
    an Android emulator which lacks the full build environment.
    
    -- Add script to set things up to match PathService (placeholder
       until we get a real recipe).
    
    Change-Id: If62ffa0d5e3ba6764a51951f1e5cbeb68addc1f7

---
+++ testing/tools/adb_push_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2026 The PDFium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# adb_push_test.sh: push files from a chromium checkout to android emulator.
+#
+# PRECONDITIONS:
+#   Android emulator already configured on host.
+#   Android binaries build into out/Android.
+#   Script is run from top-level pdfium dirctory.
+
+adb push --sync out/Android/*  /data/local/tmp/
+
+# Move test binaries up, replacing python invoker placeholders.
+adb push out/Android/pdfium_unittests__dist/pdfium_unittests /data/local/tmp/pdfium_unittests
+adb push out/Android/pdfium_embeddertests__dist/pdfium_embeddertests /data/local/tmp/pdfium_embeddertests
+
+# Now move the test resources.
+adb push --sync testing/resources /data/local/chromium_tests_root/testing
+adb push --sync third_party/NotoSansCJK/ /data/local/chromium_tests_root/third_party/NotoSansCJK
+
+# Now run the tests
+adb shell /data/local/tmp/pdfium_unittests
+adb shell /data/local/tmp/pdfium_embeddertests
