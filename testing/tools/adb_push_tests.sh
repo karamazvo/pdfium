cf74357eddfd13d59ede59875e90a8a109612be5
Tom Sepez <tsepez@google.com>
2026-02-18 17:43:43 +0000

    Add script to set up android test files up to new PathService.
    Then run the unittests and embeddertests. This is a placeholder
    until we get a real recipe.
    
    Change-Id: If62ffa0d5e3ba6764a51951f1e5cbeb68addc1f7

---
+++ testing/tools/adb_push_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright 2026 The PDFium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# adb_push_test.sh: push files from a PDFium checkout to android emulator.
+#
+# PRECONDITIONS:
+#   Android emulator already configured on host.
+#   Android binaries build into out/Android.
+#   Script is run from top-level pdfium dirctory.
+
+LOCAL_BUILD_DIR=out/Android
+REMOTE_EXE_DIR=/data/local/tmp
+REMOTE_TEST_DIR=/data/local/chromium_tests_root
+
+# `adb push` functions differently depending upon existence of remote
+# directories. Create these first so we can run on a clean emulator.
+adb shell mkdir -p ${REMOTE_TEST_DIR}/testing
+adb shell mkdir -p ${REMOTE_TEST_DIR}/third_party
+
+adb push --sync ${LOCAL_BUILD_DIR}/*  ${REMOTE_EXE_DIR}/
+
+# Move test binaries up, replacing python invoker placeholders.
+adb push ${LOCAL_BUILD_DIR}/pdfium_unittests__dist/pdfium_unittests ${REMOTE_EXE_DIR}/pdfium_unittests
+adb push ${LOCAL_BUILD_DIR}/pdfium_embeddertests__dist/pdfium_embeddertests ${REMOTE_EXE_DIR}/pdfium_embeddertests
+
+# Now move the test resources.
+adb push --sync testing/resources ${REMOTE_TEST_DIR}/testing
+adb push --sync third_party/NotoSansCJK ${REMOTE_TEST_DIR}/third_party
+adb push --sync ${LOCAL_BUILD_DIR}/test_fonts ${REMOTE_EXE_DIR}
+
+# Now run the tests
+adb shell ${REMOTE_EXE_DIR}/pdfium_unittests
+adb shell ${REMOTE_EXE_DIR}/pdfium_embeddertests
