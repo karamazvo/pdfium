748819e8319d2a973fd810fa23f61c18393cefa9
Andy Phan <andyphan@chromium.org>
2026-02-18 12:15:34 -0800

    Add prefix to font subset name (WIP)
    According to the PDF spec, 9.6.4, the font name for font subsets must
    begin with a tag followed by a plus sign (+). The tag must consist of
    six uppercase letters.
    
    in CPDF_FontSubsetter, generate a random tag to append to the font name.
    Then, override any PDF objects with font names to use the new font
    subset name.
    
    Bug: 476127152
    Change-Id: I40e96db555053ada08eebe921dff931abaf17c61

--- fpdfsdk/fpdf_save_embeddertest.cpp
+++ fpdfsdk/fpdf_save_embeddertest.cpp
@@ -389,7 +389,7 @@ TEST_F(FPDFSaveWithFontSubsetEmbedderTest, SaveWithSubsetWithNewText) {
   // font.
   EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, FPDF_SUBSET_NEW_FONTS));
   EXPECT_THAT(GetString(), StartsWith("%PDF-1.7\r\n"));
-  EXPECT_EQ(4481u, GetString().size());
+  EXPECT_EQ(4502u, GetString().size());
 
   // Verify the text is visible.
   VerifySavedDocumentWithExpectationSuffix(kSaveNewTextFilename);
