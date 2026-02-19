66f3eb7d6dd92ff8ee982c11efecb1b63fceafa4
Tom Sepez <tsepez@google.com>
2026-02-19 01:29:35 +0000

    Remove charposlist.{cpp,h}
    Once there might have been an object. Now there's just a big function
    that should be computed by the CPDF_Font itself.
    
    Change-Id: Ifbc13e51394f1b96b37701089fabf431492df55f

--- core/fpdfapi/font/cpdf_font.h
+++ core/fpdfapi/font/cpdf_font.h
@@ -23,6 +23,7 @@
 #include "core/fxcrt/retain_ptr.h"
 #include "core/fxcrt/unowned_ptr.h"
 #include "core/fxge/cfx_font.h"
+#include "core/fxge/text_char_pos.h"
 
 class CFX_DIBitmap;
 class CPDF_CIDFont;
@@ -96,6 +97,10 @@ class CPDF_Font : public Retainable, public Observable {
   virtual bool HasFontWidths() const;
 
   bool ShouldUseFont(uint32_t glyph_id, bool has_to_unicode) const;
+  std::vector<TextCharPos> GetCharPosList(
+      pdfium::span<const uint32_t> char_codes,
+      pdfium::span<const float> char_pos,
+      float font_size);
 
   ByteString GetBaseFontName() const { return base_font_name_; }
   std::optional<FX_Charset> GetSubstFontCharset() const;
