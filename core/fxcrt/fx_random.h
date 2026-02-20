e64bb0f154973c93f924e8bf93b33e7ec6764241
Andy Phan <andyphan@chromium.org>
2026-02-20 10:34:48 -0800

    Modernize FX_Random further
    Modernize FX_Random by:
    1. Renaming all abbreviated variables to more human-readable names.
    2. Updating naming style to modern convention.
    3. Changing macros to constants.
    
    Change-Id: I23bddecd739a980e2ec3101de85f2fded8c73721

--- core/fxcrt/fx_random.h
+++ core/fxcrt/fx_random.h
@@ -32,10 +32,10 @@ class FX_Random {
   uint32_t Generate();
 
  private:
-  static constexpr size_t kN = 848;
+  static constexpr size_t kStateSize = 848;
 
-  uint32_t mti_;
-  std::array<uint32_t, kN> mt_;
+  uint32_t next_index_;
+  std::array<uint32_t, kStateSize> state_;
 };
 
 #endif  // CORE_FXCRT_FX_RANDOM_H_
