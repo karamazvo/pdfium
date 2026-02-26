8921462b044d62123393788f1f1cc6bc9460392f
Andy Phan <andyphan@chromium.org>
2026-02-26 14:13:14 -0800

    Add text extraction tests for font subsetting
    Text extraction currently works for text using subsetted fonts. Since
    /ToUnicode still needs to be subsetted, add tests to avoid any
    regressions.
    
    Bug: 476127152
    Change-Id: I7429f6ac1d6f6df324f1d4967e72928b634c0a8d

--- fpdfsdk/fpdf_save_embeddertest.cpp
+++ fpdfsdk/fpdf_save_embeddertest.cpp
@@ -12,6 +12,7 @@
 #include "public/fpdf_edit.h"
 #include "public/fpdf_ppo.h"
 #include "public/fpdf_save.h"
+#include "public/fpdf_text.h"
 #include "public/fpdfview.h"
 #include "testing/embedder_test.h"
 #include "testing/embedder_test_constants.h"
@@ -21,6 +22,7 @@
 #include "testing/utils/file_util.h"
 #include "testing/utils/path_service.h"
 
+using testing::ElementsAreArray;
 using testing::HasSubstr;
 using testing::Not;
 using testing::StartsWith;
@@ -313,6 +315,26 @@ TEST_F(FPDFSaveEmbedderTest, IncrementalSaveWithModifications) {
 class FPDFSaveWithFontSubsetEmbedderTest : public FPDFSaveEmbedderTest {
  public:
   static constexpr char kSaveNewTextFilename[] = "save_new_text";
+  static constexpr std::array<FPDF_WCHAR, 39> kExpectedSingleTextAdded = {
+      // "Hello, world!\r\n"
+      'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!', '\r',
+      '\n',
+      // "Goodbye, world!\r\n"
+      'G', 'o', 'o', 'd', 'b', 'y', 'e', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!',
+      '\r', '\n',
+      // "这是第一句。"
+      0x8FD9, 0x662F, 0x7B2C, 0x2F00, 0x53E5, 0x3002, 0};
+  static constexpr std::array<FPDF_WCHAR, 53> kExpectedMultipleTextAdded = {
+      // "Hello, world!\r\n"
+      'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!', '\r',
+      '\n',
+      // "Goodbye, world!\r\n"
+      'G', 'o', 'o', 'd', 'b', 'y', 'e', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!',
+      '\r', '\n',
+      // "这是第一句。\r\n"
+      0x8FD9, 0x662F, 0x7B2C, 0x2F00, 0x53E5, 0x3002, '\r', '\n',
+      // "Hello again."
+      'H', 'e', 'l', 'l', 'o', ' ', 'a', 'g', 'a', 'i', 'n', '.', 0};
 
   ScopedFPDFFont LoadTestFont(const std::string& font_path) {
     std::vector<uint8_t> font_data = GetFileContents(font_path.c_str());
@@ -338,6 +360,27 @@ class FPDFSaveWithFontSubsetEmbedderTest : public FPDFSaveEmbedderTest {
     ASSERT_TRUE(FPDFPageObj_TransformF(text_obj, &matrix));
     FPDFPage_InsertObject(page, text_obj);
     ASSERT_TRUE(FPDFPage_GenerateContent(page));
+  }
+
+  // `expected` must include the NUL terminator.
+  void TestExtractedFont(pdfium::span<const unsigned short> expected) {
+    ScopedSavedDoc saved_doc = OpenScopedSavedDocument();
+    ASSERT_TRUE(saved_doc);
+    ScopedSavedPage saved_page = LoadScopedSavedPage(0);
+    ASSERT_TRUE(saved_page);
+    ScopedFPDFTextPage text_page(FPDFText_LoadPage(saved_page.get()));
+    ASSERT_TRUE(text_page);
+
+    int actual_count = FPDFText_CountChars(text_page.get());
+    ASSERT_EQ(static_cast<int>(expected.size() - 1), actual_count);
+
+    // Includes the NUL terminator.
+    std::vector<FPDF_WCHAR> actual_buffer(actual_count + 1);
+    ASSERT_EQ(actual_count + 1,
+              FPDFText_GetText(text_page.get(), 0, actual_count,
+                               actual_buffer.data()));
+
+    EXPECT_THAT(actual_buffer, ElementsAreArray(expected));
   }
 };
 
@@ -372,6 +415,8 @@ TEST_F(FPDFSaveWithFontSubsetEmbedderTest, SaveWithoutSubsetWithNewText) {
 
   // Verify the text is visible.
   VerifySavedDocumentWithExpectationSuffix(kSaveNewTextFilename);
+
+  TestExtractedFont(kExpectedSingleTextAdded);
 }
 
 TEST_F(FPDFSaveWithFontSubsetEmbedderTest, SaveWithSubsetWithNewText) {
@@ -398,6 +443,8 @@ TEST_F(FPDFSaveWithFontSubsetEmbedderTest, SaveWithSubsetWithNewText) {
 
   // Verify the text is visible.
   VerifySavedDocumentWithExpectationSuffix(kSaveNewTextFilename);
+
+  TestExtractedFont(kExpectedSingleTextAdded);
 }
 
 TEST_F(FPDFSaveWithFontSubsetEmbedderTest,
@@ -434,4 +481,6 @@ TEST_F(FPDFSaveWithFontSubsetEmbedderTest,
 
   // Verify the text is visible.
   VerifySavedDocumentWithExpectationSuffix(kSaveMultipleFontsFilename);
+
+  TestExtractedFont(kExpectedMultipleTextAdded);
 }
