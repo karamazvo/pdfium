648eba1b450cec7be28d8b85dc24abfc2bb11353
Tom Sepez <tsepez@google.com>
2026-02-27 18:45:07 +0000

    First CL to make a CFX_GlyphBitmap hold a SkBitmap.
    The SkBitmap is aliased with CFX_DIBitmap with the intention that
    subsequent CLs can render into it with SKIA, and PDFium can then
    composite it using the DIBitmap.
    
    -- Loosen include rules as more of fxge becomes skia-aware.
    
    Bug: 485984630
    Change-Id: Ia84159a83e75dd260f5a98d2cdf71abf1001dc3a

--- core/fxge/cfx_glyphbitmap.cpp
+++ core/fxge/cfx_glyphbitmap.cpp
@@ -6,18 +6,32 @@
 
 #include <utility>
 
+#include "core/fxcrt/check_op.h"
 #include "core/fxge/dib/cfx_dibitmap.h"
+
+#if defined(PDF_USE_SKIA)
+#include "core/fxge/cfx_defaultrenderdevice.h"
+#include "third_party/skia/include/core/SkImageInfo.h"  // nogncheck
+#endif
 
 CFX_GlyphBitmap::CFX_GlyphBitmap(int left,
                                  int top,
                                  RetainPtr<CFX_DIBitmap> bitmap)
-    : left_(left), top_(top), bitmap_(std::move(bitmap)) {}
+    : left_(left), top_(top), bitmap_(std::move(bitmap)) {
+#if defined(PDF_USE_SKIA)
+  if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
+    CHECK_EQ(bitmap_->GetBPP(), 8);
+    pdfium::span<uint8_t> bitmap_span = bitmap_->GetWritableBuffer();
+    if (!bitmap_span.empty()) {
+      sk_bitmap_.installPixels(
+          SkImageInfo::MakeA8(bitmap_->GetWidth(), bitmap_->GetHeight()),
+          bitmap_span.data(), bitmap_->GetPitch());
+    }
+  }
+#endif
+}
 
 CFX_GlyphBitmap::~CFX_GlyphBitmap() = default;
-
-RetainPtr<CFX_DIBitmap> CFX_GlyphBitmap::GetWritableBitmap() {
-  return bitmap_;
-}
 
 RetainPtr<const CFX_DIBitmap> CFX_GlyphBitmap::GetBitmap() const {
   return bitmap_;
