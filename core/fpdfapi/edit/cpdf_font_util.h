f99ebf720d9c727923f815ba50063561f9bf3727
Andy Phan <andyphan@chromium.org>
2026-02-27 09:29:40 -0800

    Move and update CreateWidthsArray()
    CreateWidthsArray() is in an anonymous namespace in fpdf_edittext.cpp.
    In https://pdfium-review.googlesource.com/c/pdfium/+/143890, it will be
    reused to override the /W entry for subsetted fonts, so move it to a
    util file. Since font subsetting does not modify the in-memory document,
    update it so it is the caller's responsibility to add it to the
    document.
    
    Bug: 476127152
    Change-Id: Ia80dd4a4e5ef9bbd46fb4dc0f325f8ec2fae52a4

--- core/fpdfapi/edit/cpdf_font_util.h
+++ core/fpdfapi/edit/cpdf_font_util.h
@@ -11,10 +11,15 @@
 
 #include "core/fxcrt/retain_ptr.h"
 
+class CPDF_Array;
 class CPDF_Stream;
 
-// Loads the charcode to unicode mapping into a stream. It is the caller's
+// Creates a widths array from a mapping of CIDs to widths. It is the caller's
 // responsibility to add it to a `CPDF_Document` if necessary.
+RetainPtr<CPDF_Array> CreateWidthsArray(
+    const std::map<uint32_t, uint32_t>& widths);
+
+// Loads the charcode to unicode mapping into a stream.
 RetainPtr<CPDF_Stream> LoadUnicode(
     const std::multimap<uint32_t, uint32_t>& to_unicode);
 
