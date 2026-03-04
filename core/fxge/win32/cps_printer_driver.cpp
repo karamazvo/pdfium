595fffb37c9ec964e1bd24589fc8f80d406bb766
Aryan <aryankrishnan4b@gmail.com>
2026-03-04 16:27:41 +0530

    Add Description Later
    Change-Id: I033ac3b0e0111c6587f43b3f1a14a624d6fe96f7

--- core/fxge/win32/cps_printer_driver.cpp
+++ core/fxge/win32/cps_printer_driver.cpp
@@ -98,23 +98,28 @@ DeviceType CPSPrinterDriver::GetDeviceType() const {
   return DeviceType::kPrinter;
 }
 
-int CPSPrinterDriver::GetDeviceCaps(int caps_id) const {
-  switch (caps_id) {
-    case FXDC_PIXEL_WIDTH:
-      return width_;
-    case FXDC_PIXEL_HEIGHT:
-      return height_;
-    case FXDC_BITS_PIXEL:
-      return bits_per_pixel_;
-    case FXDC_RENDER_CAPS:
-      return 0;
-    case FXDC_HORZ_SIZE:
-      return horz_size_;
-    case FXDC_VERT_SIZE:
-      return vert_size_;
-    default:
-      NOTREACHED();
-  }
+int CPSPrinterDriver::GetPixelWidth() const {
+  return width_;
+}
+
+int CPSPrinterDriver::GetPixelHeight() const {
+  return height_;
+}
+
+int CPSPrinterDriver::GetBitsPerPixel() const {
+  return bits_per_pixel_;
+}
+
+int CPSPrinterDriver::GetRenderCaps() const {
+  return 0;
+}
+
+int CPSPrinterDriver::GetHorzSize() const {
+  return horz_size_;
+}
+
+int CPSPrinterDriver::GetVertSize() const {
+  return vert_size_;
 }
 
 void CPSPrinterDriver::SaveState() {
