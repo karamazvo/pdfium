3482adc0c758c4d5946dfd6ca6aab9fa654f1c55
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-03-07 18:41:36 +0530

    Wrap GetDeviceCaps calls in a common header
    Centralize direct calls to the Windows API ::GetDeviceCaps() function by
    introducing a new header, win32_devicecaps.h.
    
    This change adds the w32_caps namespace and replaces existing direct
    invocations with descriptive inline wrapper functions. These wrappers
    abstract raw Win32 API constants to improve readability and maintain
    consistency with the architectural patterns used in other areas of the
    PDFium project.
    
    Change-Id: I9b731843f597c6fe7d050162bdcc8a8151526741

---
+++ core/fxge/win32/win32_devicecaps.h
@@ -0,0 +1,31 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXGE_WIN32_WIN32_DEVICECAPS_H_
+#define CORE_FXGE_WIN32_WIN32_DEVICECAPS_H_
+
+namespace w32_caps {
+
+inline int GetBitsPerPixel(HDC hDC) {
+  return ::GetDeviceCaps(hDC, BITSPIXEL);
+}
+inline int GetWidth(HDC hDC) {
+  return ::GetDeviceCaps(hDC, HORZRES);
+}
+inline int GetHeight(HDC hDC) {
+  return ::GetDeviceCaps(hDC, VERTRES);
+}
+inline int GetHorzSize(HDC hDC) {
+  return ::GetDeviceCaps(hDC, HORZSIZE);
+}
+inline int GetVertSize(HDC hDC) {
+  return ::GetDeviceCaps(hDC, VERTSIZE);
+}
+inline int GetDeviceType(HDC hDC) {
+  return ::GetDeviceCaps(hDC, TECHNOLOGY);
+}
+
+}  // namespace w32_caps
+
+#endif  // CORE_FXGE_WIN32_WIN32_DEVICECAPS_H_
