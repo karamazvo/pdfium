e428f8e50d364a728cf61e7f324edb3ac4ea1c2b
Tom Sepez <tsepez@google.com>
2026-03-09 17:05:19 +0000

    Simplify logic in CXFA_FFNotify::OnValueChanged()
    Follow-on to CL at
    
    Bug: 489144038
    Change-Id: I3ec9644c481cb0b2dae8b162b4b78c33890b1a5c

--- xfa/fxfa/cxfa_ffnotify.cpp
+++ xfa/fxfa/cxfa_ffnotify.cpp
@@ -448,13 +448,16 @@ void CXFA_FFNotify::OnValueChanged(CXFA_Node* pSender,
 
   if (eAttr == XFA_Attribute::Value) {
     pDocView->AddCalculateNodeNotify(pSender);
-    if (eType == XFA_Element::Value || bIsContainerNode) {
-      if (bIsContainerNode) {
-        doc_->GetDocView()->UpdateUIDisplay(pWidgetNode, nullptr);
-        pDocView->AddCalculateNode(pWidgetNode);
-        pDocView->AddValidateNode(pWidgetNode);
-      } else if (pWidgetNode->GetParent()->GetElementType() ==
-                 XFA_Element::ExclGroup) {
+    if (bIsContainerNode) {
+      doc_->GetDocView()->UpdateUIDisplay(pWidgetNode, nullptr);
+      pDocView->AddCalculateNode(pWidgetNode);
+      pDocView->AddValidateNode(pWidgetNode);
+      return;
+    }
+    if (eType == XFA_Element::Value) {
+      CXFA_Node* widget_parent = pWidgetNode->GetParent();
+      if (widget_parent &&
+          widget_parent->GetElementType() == XFA_Element::ExclGroup) {
         doc_->GetDocView()->UpdateUIDisplay(pWidgetNode, nullptr);
       }
       return;
