5641e1add32c1f2216572298dc683de1d6a84747
Tom Sepez <tsepez@google.com>
2026-03-11 21:54:24 +0000

    Consolidate more font loading code.
    Change-Id: I8d7de4c5c56aa2bedb549001c97de90521d00b82

--- core/fxge/cfx_font.cpp
+++ core/fxge/cfx_font.cpp
@@ -173,8 +173,7 @@ void CFX_Font::SetFaceFromFont(const CFX_Font& that) {
 void CFX_Font::SetSubstFont(std::unique_ptr<CFX_SubstFont> subst) {
   subst_font_ = std::move(subst);
 }
-#endif  // !BUILDFLAG(IS_WIN)
-#endif  // PDF_ENABLE_XFA
+#endif  // defined(PDF_ENABLE_XFA) & !BUILDFLAG(IS_WIN)
 
 CFX_Font::~CFX_Font() {
   font_data_ = {};  // font_data_ can't outive face_.
@@ -185,15 +184,23 @@ CFX_Font::~CFX_Font() {
 #endif
 }
 
-bool CFX_Font::LoadFaceFromSpan(pdfium::span<const uint8_t> src_span,
-                                bool force_vertical,
-                                uint64_t object_tag) {
+bool CFX_Font::LoadFaceZeroFromSpan(pdfium::span<const uint8_t> src_span,
+                                    bool force_vertical,
+                                    uint64_t object_tag) {
   vertical_ = force_vertical;
-  object_tag_ = object_tag;
   font_data_allocation_ = DataVector<uint8_t>(src_span.begin(), src_span.end());
-  face_ = CFX_Face::New(
-      pdfium::MakeRetain<CFX_ReadOnlySpanStream>(font_data_allocation_), 0);
   font_data_ = font_data_allocation_;
+  return LoadFaceFromSpanStream(
+      pdfium::MakeRetain<CFX_ReadOnlySpanStream>(font_data_allocation_), 0,
+      object_tag);
+}
+
+bool CFX_Font::LoadFaceFromSpanStream(
+    const RetainPtr<CFX_ReadOnlySpanStream>& stream,
+    int face_index,
+    uint64_t object_tag) {
+  object_tag_ = object_tag;
+  face_ = CFX_Face::New(CFX_GEModule::Get()->GetFontMgr(), stream, face_index);
   return !!face_;
 }
 
