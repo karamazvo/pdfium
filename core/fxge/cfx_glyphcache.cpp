ba97b408c0efc0d06918757a3bbed2c95c8709ba
Tom Sepez <tsepez@google.com>
2026-03-12 20:58:26 +0000

    Move renderer selection from CFX_DefaultRenderDevice to CFX_GEModule.
    Consolidate initialization into a single CFX_GEModule::Create() call.
    This centralizes global engine state and simplifies the library
    initialization workflow.
    
    -- Avoid runtime test for skia if not compiled into PDFium.
    
    Change-Id: I7c737e83969d3e2aaaf9528698739fa1392212b2

--- core/fxge/cfx_glyphcache.cpp
+++ core/fxge/cfx_glyphcache.cpp
@@ -14,8 +14,8 @@
 #include "core/fxcrt/fx_codepage.h"
 #include "core/fxcrt/span.h"
 #include "core/fxcrt/to_underlying.h"
-#include "core/fxge/cfx_defaultrenderdevice.h"
 #include "core/fxge/cfx_font.h"
+#include "core/fxge/cfx_gemodule.h"
 #include "core/fxge/cfx_glyphbitmap.h"
 #include "core/fxge/cfx_path.h"
 #include "core/fxge/cfx_substfont.h"
@@ -161,7 +161,7 @@ const CFX_GlyphBitmap* CFX_GlyphCache::LoadGlyphBitmap(
 
 #if BUILDFLAG(IS_APPLE)
   const bool bDoLookUp =
-      !text_options->native_text || CFX_DefaultRenderDevice::UseSkiaRenderer();
+      !text_options->native_text || CFX_GEModule::Get()->UseSkiaRenderer();
 #else
   const bool bDoLookUp = true;
 #endif
@@ -171,7 +171,7 @@ const CFX_GlyphBitmap* CFX_GlyphCache::LoadGlyphBitmap(
   }
 
 #if BUILDFLAG(IS_APPLE)
-  DCHECK(!CFX_DefaultRenderDevice::UseSkiaRenderer());
+  DCHECK(!CFX_GEModule::Get()->UseSkiaRenderer());
 
   auto it = size_map_.find(FaceGlyphsKey);
   if (it != size_map_.end()) {
