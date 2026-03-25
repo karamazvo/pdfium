b598ce5b3be2143675f2ee184abc72f59546770f
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-03-25 11:24:21 +0530

    Replace macro-based modifiers with WSTRModifiers enum
    Introduce a WSTRModifiers enum to replace the legacy FORCE_ANSI,
    FORCE_UNICODE, and FORCE_INT64 macros in WideString. Using a structured
    type instead of bitwise OR operations on integer flags improves type
    safety and code readability.
    
    The string formatting logic is refactored to use these boolean flags.
    Specifically, the handling of character and string types ('c', 'C', 's',
    'S') is consolidated. By determining the width of the expected argument
    (wide vs. narrow) based on the format character and the modifiers
    upfront, the code eliminates redundant switch cases and reduces
    duplication when calculating item lengths.
    
    Change-Id: I7e11eeea797590fca3c59260bf5c98f10567ff8d

--- core/fxcrt/widestring.cpp
+++ core/fxcrt/widestring.cpp
@@ -32,9 +32,12 @@ template class fxcrt::StringViewTemplate<wchar_t>;
 template class fxcrt::StringPoolTemplate<WideString>;
 template struct std::hash<WideString>;
 
-#define FORCE_ANSI 0x10000
-#define FORCE_UNICODE 0x20000
-#define FORCE_INT64 0x40000
+enum class WSTRModifier {
+  kForceNone,
+  kForceAnsi,
+  kForceUnicode,
+  kForceInt64,
+};
 
 namespace {
 
@@ -109,18 +112,18 @@ std::optional<size_t> GuessSizeForVSWPrintf(const wchar_t* pFormat,
       return std::nullopt;
     }
     uint32_t nPrecision = static_cast<uint32_t>(iPrecision);
-    int nModifier = 0;
+    WSTRModifier nModifier = WSTRModifier::kForceNone;
     if (view.First(3u) == L"I64") {
       view = view.Substr(3u);
-      nModifier = FORCE_INT64;
+      nModifier = WSTRModifier::kForceInt64;
     } else {
       switch (view.Front()) {
         case 'h':
-          nModifier = FORCE_ANSI;
+          nModifier = WSTRModifier::kForceAnsi;
           view = view.Substr(1u);
           break;
         case 'l':
-          nModifier = FORCE_UNICODE;
+          nModifier = WSTRModifier::kForceUnicode;
           view = view.Substr(1u);
           break;
         case 'F':
@@ -131,68 +134,30 @@ std::optional<size_t> GuessSizeForVSWPrintf(const wchar_t* pFormat,
       }
     }
     size_t nItemLen = 0;
-    switch (view.Front() | nModifier) {
+    switch (view.Front()) {
       case 'c':
       case 'C':
         nItemLen = 2;
         va_arg(argList, int);
         break;
-      case 'c' | FORCE_ANSI:
-      case 'C' | FORCE_ANSI:
-        nItemLen = 2;
-        va_arg(argList, int);
-        break;
-      case 'c' | FORCE_UNICODE:
-      case 'C' | FORCE_UNICODE:
-        nItemLen = 2;
-        va_arg(argList, int);
-        break;
-      case 's': {
-        const wchar_t* pstrNextArg = va_arg(argList, const wchar_t*);
-        if (pstrNextArg) {
-          nItemLen = wcslen(pstrNextArg);
-          if (nItemLen < 1) {
-            nItemLen = 1;
-          }
-        } else {
-          nItemLen = 6;
-        }
-      } break;
+      case 's':
       case 'S': {
-        const char* pstrNextArg = va_arg(argList, const char*);
-        if (pstrNextArg) {
-          nItemLen = strlen(pstrNextArg);
-          if (nItemLen < 1) {
-            nItemLen = 1;
-          }
-        } else {
-          nItemLen = 6;
+        bool wide = (view.Front() == 's');
+        if (nModifier == WSTRModifier::kForceUnicode) {
+          wide = true;
+        } else if (nModifier == WSTRModifier::kForceAnsi) {
+          wide = false;
         }
-      } break;
-      case 's' | FORCE_ANSI:
-      case 'S' | FORCE_ANSI: {
-        const char* pstrNextArg = va_arg(argList, const char*);
-        if (pstrNextArg) {
-          nItemLen = strlen(pstrNextArg);
-          if (nItemLen < 1) {
-            nItemLen = 1;
-          }
+
+        if (wide) {
+          const wchar_t* pstrNextArg = va_arg(argList, const wchar_t*);
+          nItemLen = pstrNextArg ? std::max<size_t>(1, wcslen(pstrNextArg)) : 6;
         } else {
-          nItemLen = 6;
+          const char* pstrNextArg = va_arg(argList, const char*);
+          nItemLen = pstrNextArg ? std::max<size_t>(1, strlen(pstrNextArg)) : 6;
         }
-      } break;
-      case 's' | FORCE_UNICODE:
-      case 'S' | FORCE_UNICODE: {
-        const wchar_t* pstrNextArg = va_arg(argList, wchar_t*);
-        if (pstrNextArg) {
-          nItemLen = wcslen(pstrNextArg);
-          if (nItemLen < 1) {
-            nItemLen = 1;
-          }
-        } else {
-          nItemLen = 6;
-        }
-      } break;
+        break;
+      }
     }
     if (nItemLen != 0) {
       if (nPrecision != 0 && nItemLen > nPrecision) {
@@ -209,7 +174,7 @@ std::optional<size_t> GuessSizeForVSWPrintf(const wchar_t* pFormat,
         case 'x':
         case 'X':
         case 'o':
-          if (nModifier & FORCE_INT64) {
+          if (nModifier == WSTRModifier::kForceInt64) {
             va_arg(argList, int64_t);
           } else {
             va_arg(argList, int);
