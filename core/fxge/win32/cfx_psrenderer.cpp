a63a17f00e7e40e20bd0f9fbeaffec4e48e21acb
Tom Sepez <tsepez@google.com>
2026-04-06 18:06:35 +0000

    Use safe arithmetic in CFX_PSRenderer::DrawDIBits()
    Hardening suggestion from the bot.
    
    Bug: 500036290
    Change-Id: Ie521629d06ba944f610b941a8c9e9505fa29aea7

--- core/fxge/win32/cfx_psrenderer.cpp
+++ core/fxge/win32/cfx_psrenderer.cpp
@@ -620,8 +620,16 @@ bool CFX_PSRenderer::DrawDIBits(RetainPtr<const CFX_DIBBase> bitmap,
         encoder_iface_->pJpegEncodeFunc(bitmap, &output_buf, &output_size)) {
       filter = "/DCTDecode filter ";
     } else {
-      int src_pitch = width * bytes_per_pixel;
-      output_size = height * src_pitch;
+      FX_SAFE_UINT32 safe_output_size = bytes_per_pixel;
+      safe_output_size *= width;
+      safe_output_size *= height;
+      if (!safe_output_size.IsValid()) {
+        WriteString("\nQ\n");
+        return false;
+      }
+      output_size = safe_output_size.ValueOrDie();
+      // Valid `output_size` implies valid `src_pitch`.
+      uint32_t src_pitch = width * bytes_per_pixel;
       output_buf = FX_Alloc(uint8_t, output_size);
       for (int row = 0; row < height; row++) {
         const uint8_t* src_scan = bitmap->GetScanline(row).data();
