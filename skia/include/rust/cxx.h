aa29d9fbca1d129a84464f6bf25ea04674e1ee1a
Tom Sepez <tsepez@google.com>
2026-04-16 23:48:44 +0000

    Build example skrifa_cxx bridge.
    Gemini-integrated build of a forthcoming fontations patch.
    
    Change-Id: I4e616cbd150f6aa2a3e7d1169148d3bf2a7d8468

---
+++ skia/include/rust/cxx.h
@@ -0,0 +1,5 @@
+// Copyright 2026 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/rust/cxx/v1/cxx.h"
