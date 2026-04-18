1939bef4ea656570f5a91c02ca323cc68c1ac889
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-04-18 17:27:26 +0530

    Dashboard for all core/fdrm changes
    [DO NOT LAND]
    This cl is merely to act as a dashboard for things that should be changed in core/fdrm directory. Please add unresolved comments for things that can be improved in the directory and use this list to refer to things that can be changed when making a CL to clean up the directory.
    
    Bug: 503171645
    Change-Id: I34b1c28b68632a44780e5428f7da9e04ddcf2658

--- core/fdrm/fx_crypt_sha.h
+++ core/fdrm/fx_crypt_sha.h
@@ -1,6 +1,6 @@
 // Copyright 2024 The PDFium Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file .
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
--- core/fdrm/fx_crypt_unittest.cpp
+++ core/fdrm/fx_crypt_unittest.cpp
@@ -1,6 +1,6 @@
 // Copyright 2016 The PDFium Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file .
 
 #include "core/fdrm/fx_crypt.h"
 
