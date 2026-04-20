1c9d4dceac5918512feb8df47dfcd1569e624d58
Tom Sepez <tsepez@google.com>
2026-04-20 23:27:27 +0000

    Keep persistent mapping in CFXJSE_Engine::NewNormalXFAObject().
    -- Update embedder test framework for both timers and CPPGC.
    
    Bug: 504416752
    Change-Id: Iac5c0ca57fdf3cca8ea9a3703180bed847abe1c5

--- fxjs/xfa/cjx_list_embeddertest.cpp
+++ fxjs/xfa/cjx_list_embeddertest.cpp
@@ -2,7 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "fpdfsdk/cpdfsdk_helpers.h"
+#include "fpdfsdk/fpdfxfa/cpdfxfa_context.h"
+#include "fxjs/gc/heap.h"
+#include "testing/embedder_test_environment.h"
+#include "testing/embedder_test_timer_handling_delegate.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "testing/v8_test_environment.h"
 #include "testing/xfa_js_embedder_test.h"
 
 class CJXListEmbedderTest : public XFAJSEmbedderTest {};
@@ -12,4 +18,23 @@ TEST_F(CJXListEmbedderTest, Bug1263) {
   ASSERT_TRUE(OpenDocument("simple_xfa.pdf"));
 
   EXPECT_FALSE(Execute("nodes.insert($form,$)"));
+}
+
+// Should not crash.
+TEST_F(CJXListEmbedderTest, Bug504416752) {
+  EmbedderTestTimerHandlingDelegate delegate;
+  SetDelegate(&delegate);
+
+  ASSERT_TRUE(OpenDocument("bug_504416752.pdf"));
+  ScopedPage page = LoadScopedPage(0);
+  ASSERT_TRUE(page);
+  DoOpenActions();
+
+  ForceCppGarbageCollection();
+
+  for (int i = 0; i < 100000; ++i) {
+    delegate.AdvanceTime(1);
+    V8TestEnvironment::PumpPlatformMessageLoop(
+        V8TestEnvironment::GetInstance()->isolate());
+  }
 }
