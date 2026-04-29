71b5b078ab74030a97c87f4ea98d8624c98da68c
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-04-29 22:27:57 +0530

    Replace some char pointers with ByteStrings in embedder tests
    Update the embedder test helpers to use ByteString instead of raw char*
    pointers for password parameters. This modernization improves type
    safety and consistency across the testing infrastructure.
    
    Modified methods include:
    - OpenDocumentWithPassword()
    - OpenDocumentWithOptions()
    - OpenDocumentHelper()
    - OpenScopedSavedDocumentWithPassword()
    - OpenSavedDocumentWithPassword()
    
    The implementation now utilizes password.c_str() when passing values to
    underlying FPDF functions that still require C-style strings.
    
    Bug: 42270715
    Change-Id: I81caca26114770a633a185e361958a0a322ca160

--- testing/embedder_test.h
+++ testing/embedder_test.h
@@ -15,6 +15,7 @@
 #include <vector>
 
 #include "build/build_config.h"
+#include "core/fxcrt/bytestring.h"
 #include "core/fxcrt/span.h"
 #include "core/fxcrt/unowned_ptr.h"
 #include "public/cpp/fpdf_scopers.h"
@@ -97,7 +98,7 @@ class EmbedderTest : public ::testing::Test,
    public:
     ScopedSavedDoc();
     explicit ScopedSavedDoc(EmbedderTest* test);
-    ScopedSavedDoc(EmbedderTest* test, const char* password);
+    ScopedSavedDoc(EmbedderTest* test, const ByteString& password);
     ScopedSavedDoc(const ScopedSavedDoc&) = delete;
     ScopedSavedDoc& operator=(const ScopedSavedDoc&) = delete;
     ScopedSavedDoc(ScopedSavedDoc&&) noexcept;
@@ -187,7 +188,7 @@ class EmbedderTest : public ::testing::Test,
   // is kDisableJavascript, then the document will be given stubs in place
   // of the real JS engine.
   virtual bool OpenDocumentWithOptions(const std::string& filename,
-                                       const char* password,
+                                       const ByteString& password,
                                        LinearizeOption linearize_option,
                                        JavaScriptOption javascript_option);
 
@@ -195,7 +196,7 @@ class EmbedderTest : public ::testing::Test,
   bool OpenDocument(const std::string& filename);
   bool OpenDocumentLinearized(const std::string& filename);
   bool OpenDocumentWithPassword(const std::string& filename,
-                                const char* password);
+                                const ByteString& password);
   bool OpenDocumentWithoutJavaScript(const std::string& filename);
 
   // Close the document from a previous OpenDocument() call. This happens
@@ -290,7 +291,7 @@ class EmbedderTest : public ::testing::Test,
  protected:
   using PageNumberToHandleMap = std::map<int, FPDF_PAGE>;
 
-  bool OpenDocumentHelper(const char* password,
+  bool OpenDocumentHelper(const ByteString& password,
                           LinearizeOption linearize_option,
                           JavaScriptOption javascript_option,
                           FakeFileAccess* network_simulator,
@@ -355,7 +356,8 @@ class EmbedderTest : public ::testing::Test,
 
   // See comments in the respective non-Saved versions of these methods.
   ScopedSavedDoc OpenScopedSavedDocument();
-  ScopedSavedDoc OpenScopedSavedDocumentWithPassword(const char* password);
+  ScopedSavedDoc OpenScopedSavedDocumentWithPassword(
+      const ByteString& password);
   ScopedSavedPage LoadScopedSavedPage(int page_index);
   FPDF_PAGE LoadSavedPage(int page_index);
   void CloseSavedPage(FPDF_PAGE page);
@@ -406,7 +408,7 @@ class EmbedderTest : public ::testing::Test,
   // only. Callers should use the Scoped methods that manage lifetime
   // automatically.
   FPDF_DOCUMENT OpenSavedDocument();
-  FPDF_DOCUMENT OpenSavedDocumentWithPassword(const char* password);
+  FPDF_DOCUMENT OpenSavedDocumentWithPassword(const ByteString& password);
 
   // Closes a document opened via OpenSavedDocument(). This must only be invoked
   // in documents opened by the helpers above. This is intended for internal use
