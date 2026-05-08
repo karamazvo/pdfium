42ffa00094368b8cfb02b4117b0ed8c07d5d2af3
Lei Zhang <thestig@chromium.org>
2026-05-08 10:57:28 -0700

    Make some fxge code Windows / AGG-only
    Make CFX_RenderDevice::FillRect() fallback code Windows-only, since only
    CGdiDeviceDriver can trigger this code. All other drivers that implement
    RenderDeviceDriverIface::FillRect() will always succeed. After this,
    CFX_DIBitmap::CompositeRect() becomes Windows/AGG-only.
    
    Change-Id: I20530d463ec968ae0cdd68b3aa40cccf09568e27

--- core/fxge/dib/cfx_dibitmap.cpp
+++ core/fxge/dib/cfx_dibitmap.cpp
@@ -739,6 +739,7 @@ void CFX_DIBitmap::CompositeOneBPPMask(int dest_left,
   });
 }
 
+#if BUILDFLAG(IS_WIN) || defined(PDF_USE_AGG)
 bool CFX_DIBitmap::CompositeRect(int left,
                                  int top,
                                  int width,
@@ -910,6 +911,7 @@ bool CFX_DIBitmap::CompositeRect(int left,
   }
   return true;
 }
+#endif  // BUILDFLAG(IS_WIN) || defined(PDF_USE_AGG)
 
 bool CFX_DIBitmap::ConvertFormat(FXDIB_Format dest_format) {
   static constexpr FXDIB_Format kAllowedDestFormats[] = {
