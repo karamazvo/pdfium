de7aa5d483c1f277561daab78570ea01adbdcb3f
Andy Phan <andyphan@chromium.org>
2026-05-13 12:36:18 -0700

    Roll abseil, build, buildtools, clang, libc++, rust and tools/rust
    Roll third_party/abseil-cpp/ dd4432fb0..5e42a36a8 (8 commits)
    
    https://chromium.googlesource.com/chromium/src/third_party/abseil-cpp.git/+log/dd4432fb0258..5e42a36a85a2
    
    Created with:
      roll-dep third_party/abseil-cpp
    
    Roll build/ 08cae1166..8a46ede8e (104 commits)
    
    https://chromium.googlesource.com/chromium/src/build.git/+log/08cae1166f77..8a46ede8ea86
    
    Created with:
      roll-dep build
    
    Roll buildtools/ 11cc2bd83..5e7f45b2c (21 commits)
    
    https://chromium.googlesource.com/chromium/src/buildtools.git/+log/11cc2bd83053..5e7f45b2c8d0
    
    Created with:
      roll-dep buildtools
    
    Roll tools/clang/ 772ec0f7d..3e520fe28 (16 commits)
    
    https://chromium.googlesource.com/chromium/src/tools/clang/+log/772ec0f7db79..3e520fe28bbe
    
    Roll clang+rust llvmorg-23-init-5669-g8a0be0bc-4 : llvmorg-23-init-10931-g20b6ec66-8
    
    Created with:
      roll-dep tools/clang
    
    Roll third_party/libc++/src/ 7ab65651a..be1c391ac (324 commits)
    
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git/+log/7ab65651aed6..be1c391acca0
    
    Created with:
      roll-dep third_party/libc++/src
    
    Roll third_party/rust/ d403e286a..dbc491d22 (24 commits)
    
    https://chromium.googlesource.com/chromium/src/third_party/rust/+log/d403e286a47d..dbc491d22ee3
    
    Created with:
      roll-dep third_party/rust
    
    Roll tools/rust/ 69bb79a13..4442fd386 (5 commits)
    
    https://chromium.googlesource.com/chromium/src/tools/rust/+log/69bb79a132d3..4442fd3860a2
    
    Created with:
      roll-dep tools/rust
    
    Change-Id: Icd4f9b4ffd073bd9bb66d816e94926aa48cf5267

--- tools/roll_pdfium_deps_test.py
+++ tools/roll_pdfium_deps_test.py
@@ -49,6 +49,7 @@ vars = {
   # purposes, by adding the following line to src.git's .gclient entry:
   #      "custom_vars": { "checkout_configuration": "small" },
   'checkout_configuration': 'default',
+  'android_ndk_version': Str('2@30.0.14608247'),
 
   # By default, don't check out android. Will be overridden by gclient
   # variables.
@@ -974,7 +975,7 @@ deps = {
       'packages': [
             {
                 'package': 'chromium/third_party/android_toolchain/android_toolchain',
-                'version': 'KXOia11cm9lVdUdPlbGLu8sCz6Y4ey_HV2s8_8qeqhgC',
+                'version': 'version:' + Var('android_ndk_version'),
             },
       ],
       'condition': 'checkout_android and non_git_source',
@@ -1496,6 +1497,7 @@ use_relative_paths = True
 
 gclient_gn_args_file = 'build/config/gclient_args.gni'
 gclient_gn_args = [
+  'android_ndk_version',
   'build_with_chromium',
   'checkout_android',
   'checkout_skia',
@@ -1567,9 +1569,9 @@ vars = {
   # and whatever else without interference from each other.
   'abseil_revision': '14a5b78f39684f85a19690e63c722d6bb680f2cd',
   # Three lines of non-changing comments so that
-  # the commit queue can handle CLs rolling android_toolchain
+  # the commit queue can handle CLs rolling android_ndk
   # and whatever else without interference from each other.
-  'android_toolchain_version': 'KXOia11cm9lVdUdPlbGLu8sCz6Y4ey_HV2s8_8qeqhgC',
+  'android_ndk_version': Str('2@30.0.14608247'),
   # Three lines of non-changing comments so that
   # the commit queue can handle CLs rolling brotli
   # and whatever else without interference from each other.
@@ -1837,7 +1839,7 @@ deps = {
     'packages': [
       {
         'package': 'chromium/third_party/android_toolchain/android_toolchain',
-        'version': Var('android_toolchain_version'),
+        'version': 'version:' + Var('android_ndk_version'),
       },
     ],
     'condition': 'checkout_android_native_support',
@@ -2401,7 +2403,7 @@ class RollDepTest(unittest.TestCase):
 
   def testCipdDepsRollsNothingToRoll(self):
     success, message = roll_dep(CHROMIUM_DEPS, PDFIUM_DEPS,
-                                'android_toolchain_version')
+                                'android_ndk_version')
     self.assertTrue(success)
     self.assertEqual('Revisions are the same.', message)
 
